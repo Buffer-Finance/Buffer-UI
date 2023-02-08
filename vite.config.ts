cb56dec6975281f5f1fc584b8b6323e8c646a10f
bufferfinance <heisenberg@buffer.finance>
2023-02-08 11:19:37 +0530

    lint: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
-import { defineConfig,loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
-
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     // vite config
-      plugins: [react(),viteTsconfigPaths()],
+    plugins: [react(), viteTsconfigPaths()],
 
     define: {
-      __APP_ENV__: env.APP_ENV
-    }
-  }
-})
+      __APP_ENV__: env.APP_ENV,
+    },
+  };
+});
