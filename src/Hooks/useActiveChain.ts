d8a5cae42e934b911e1668cbc603830a99b2559a
Wer Heisenberg <heisenberg@buffer.finance>
2023-02-08 19:06:00 +0530

    Added default chain

--- src/Hooks/useActiveChain.ts
+++ src/Hooks/useActiveChain.ts
@@ -2,16 +2,18 @@ import { useEffect, useMemo } from 'react';
 import { getChains } from 'src/Config/wagmiClient';
 import { Chain, useNetwork } from 'wagmi';
 import Config from 'public/config.json';
+import { arbitrumGoerli } from 'wagmi/chains';
 const typeofConfig = Config[421613];
 
 
 export const useActiveChain = () => {
   const {chain} = useNetwork();
+  console.log(`chain: `,chain);
   const [activeChain, isWrongChain, configContracts] = useMemo<[Chain,boolean,typeof typeofConfig]>(() => {
     return [
-      chain,
+      chain || arbitrumGoerli,
       false,
-      Config[chain.id || '421613'] as typeof typeofConfig,
+      Config[chain?.id || '421613'] as typeof typeofConfig,
     ];
   }, [chain]);
  
