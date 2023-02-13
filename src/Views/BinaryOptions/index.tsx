eb1af2987c35db4c44e54964b59c9a8707c4c1dc
Wer Heisenberg <heisenberg@buffer.finance>
2023-02-13 14:59:06 +0530

    Made ETH as default pair

--- src/Views/BinaryOptions/index.tsx
+++ src/Views/BinaryOptions/index.tsx
@@ -111,7 +111,7 @@ export const useQTinfo = () => {
   const {activeChain} = useActiveChain();
   const data = useMemo(() => {
     let activeMarket = Config[ENV].pairs.find((m) => {
-      let market = params?.market || 'GBP-USD';
+      let market = params?.market || 'ETH-USD';
       // GBP
       market = market.toUpperCase();
       let currM = m.pair.toUpperCase();
