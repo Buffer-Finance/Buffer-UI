ffcf96b6bbe221e513475d76e90826c4b628602b
bufferfinance <heisenberg@buffer.finance>
2023-02-13 19:27:36 +0530

    Add history tables

--- src/Views/BinaryOptions/Tables/index.tsx
+++ src/Views/BinaryOptions/Tables/index.tsx
@@ -1,21 +1,18 @@
-import { ChangeEvent } from "react";
-import BufferDisclaimer from "@Views/Common/BufferDisclaimer";
-import { IQTrade } from "..";
-import PGDesktopTables from "./Desktop";
+import { ChangeEvent } from 'react';
+import { IQTrade } from '..';
+import PGDesktopTables from './Desktop';
 
 interface IPGTables {
   configData: IQTrade;
   count?: number;
   currentPage?: number;
   onPageChange?: (e: ChangeEvent, p: number) => void;
-  className?: string;
   isHistoryTable?: boolean;
   shouldFetchOldData?: boolean;
 }
 
 const PGTables: React.FC<IPGTables> = ({
   configData,
-  className,
   count,
   onPageChange,
   currentPage,
@@ -24,11 +21,6 @@ const PGTables: React.FC<IPGTables> = ({
 }) => {
   return (
     <>
-      {/* <BufferDisclaimer
-        content={
-          "Due to network congestion the changes will show up in some time (<1min)"
-        }
-      /> */}
       <PGDesktopTables
         className=""
         isHistoryTable={isHistoryTable}
