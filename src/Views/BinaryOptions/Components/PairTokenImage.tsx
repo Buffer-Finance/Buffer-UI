fe6f45df9caa23a46f359c098825ae767483c912
bufferfinance <heisenberg@buffer.finance>
2023-02-19 09:43:58 +0530

    fix in pairtokenimage

--- src/Views/BinaryOptions/Components/PairTokenImage.tsx
+++ src/Views/BinaryOptions/Components/PairTokenImage.tsx
@@ -1,27 +1,32 @@
 export const PairTokenImage = ({
   pair,
-  size = 25,
+  size = 20,
 }: {
   pair: string;
   size?: number;
 }) => {
   const [token1, token2] = pair.split('-');
+  const shouldShowSecondImage = token2.toLowerCase() !== 'usd';
   const imageSrc =
     'https://cdn.buffer.finance/Buffer-Website-Data/main/Assets/';
   return (
-    <div className="flex items-center">
+    <div
+      className={`flex items-center ${!shouldShowSecondImage ? 'mr-3' : ''}`}
+    >
       <img
         src={imageSrc + token1.toLowerCase() + '.svg'}
         className="relative z-10"
         width={size}
         height={size}
       />
-      <img
-        src={imageSrc + token2.toLowerCase() + '.svg'}
-        className="relative z-0 -left-3"
-        width={size}
-        height={size}
-      />
+      {shouldShowSecondImage && (
+        <img
+          src={imageSrc + token2.toLowerCase() + '.svg'}
+          className="relative z-0 -left-[15%]"
+          width={size}
+          height={size}
+        />
+      )}
     </div>
   );
 };
