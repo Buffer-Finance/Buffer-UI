33a86759b4728ba2343d565c713f4db04b3cf86b
Faraday26 <faraday@buffer.finance>
2023-02-20 19:45:47 +0530

    fix start timestamp

--- src/Views/V2-Leaderboard/Weekly/config.ts
+++ src/Views/V2-Leaderboard/Weekly/config.ts
@@ -2,8 +2,8 @@ type startTimestampObj = { [key: number]: number | undefined };
 type endTimestampObj = { [key: number]: number | undefined | null };
 
 export const startTimestamp: startTimestampObj = {
-  421613: 1676887200000,
-  42161: 1676887200000,
+  421613: 1676908800000,
+  42161: 1676908800000,
 };
 export const endDay: endTimestampObj = {
   421613: null,
