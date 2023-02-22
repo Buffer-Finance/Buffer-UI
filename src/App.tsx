87e0f39e6c5592464fdd6c3c5020a89b10d1478a
maxwell-1999 <maxwell@buffer.finance>
2023-02-22 20:11:02 +0530

    Added toast for referral code

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ import { Integrations } from '@sentry/tracing';
 import { Weekly } from '@Views/V2-Leaderboard/Weekly';
 import { LeaderBoardOutlet } from '@Views/V2-Leaderboard';
 import { useEffect } from 'react';
+import { useToast } from '@Contexts/Toast';
 
 if (import.meta.env.VITE_MODE === 'production') {
   console.log(`import.meta.env.SENTRY_DSN: `, import.meta.env.VITE_SENTRY_DSN);
@@ -50,11 +51,16 @@ const AppRoutes = () => {
   const activeMarketFromStorage = useAtomValue(activeMarketFromStorageAtom)
   const [searchParam] = useSearchParams();
   const [ref, setRef] = useAtom(referralCodeAtom);
-
+const toastify = useToast();
   useEffect(() => {
     const referralCode = searchParam.get('ref');
     if (referralCode) {
       if (ref !== referralCode) setRef(referralCode);
+      toastify({
+        type:'success',
+        msg:'Referral Link  "'+referralCode+'" is applied successfully!',
+        id:23132
+      })
     }
   }, [searchParam]);
   return (
