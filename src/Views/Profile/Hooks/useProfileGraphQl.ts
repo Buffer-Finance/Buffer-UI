e2a787386b66d5099f3cb398f1f942285194a077
Faraday26 <faraday@buffer.finance>
2023-02-24 18:14:01 +0530

    add most traded asset on profile

--- src/Views/Profile/Hooks/useProfileGraphQl.ts
+++ src/Views/Profile/Hooks/useProfileGraphQl.ts
@@ -7,6 +7,9 @@ import useSWR from 'swr';
 
 interface ProfileGraphQlResponse {
   userOptionDatas: {
+    optionContract: {
+      address: string;
+    };
     payout: string | null;
     totalFee: string;
   }[];
@@ -14,15 +17,16 @@ interface ProfileGraphQlResponse {
     totalFee: string;
   }[];
 }
-
-export interface ItradingMetricsData {
-  totalTrades: number;
-  net_pnl: string;
-  openInterest: string;
-  totalPayout: string;
+type metricsData = {
+  tradesPerAsset: { [key: string]: number };
   tradeWon: number;
   volume: string;
-}
+  totalPayout: string;
+  net_pnl: string;
+};
+type ItradingMetricsData = metricsData & {
+  totalTrades: number;
+};
 
 export const useProfileGraphQl = () => {
   const { address: account } = useUserAccount();
@@ -33,6 +37,9 @@ export const useProfileGraphQl = () => {
             userOptionDatas(  
               first: 1000 
               where: {user: "${account}", state_not: 1}) {
+                optionContract {
+                  address
+                }
                 payout
                 totalFee
               }
@@ -44,7 +51,7 @@ export const useProfileGraphQl = () => {
           }`,
       });
 
-      console.log(response, 'response');
+      // console.log(response, 'response');
       return response.data?.data as ProfileGraphQlResponse;
     },
     refreshInterval: 300,
@@ -57,17 +64,30 @@ export const useProfileGraphQl = () => {
     const computedData = data.userOptionDatas.reduce(
       (accumulator, currentValue) => {
         let newData = accumulator;
+
+        //increase counter for the asset contract address in tradesPerAsset object
+        const assetAddress = currentValue.optionContract.address;
+        if (newData.tradesPerAsset[assetAddress] !== undefined) {
+          newData.tradesPerAsset[assetAddress] += 1;
+        } else {
+          newData.tradesPerAsset[assetAddress] = 1;
+        }
+
+        // increase the number of trades won and totalPayout if payout is not null -->trade won
         if (currentValue.payout) {
           newData.totalPayout = add(
             accumulator.totalPayout,
             currentValue.payout
           );
           newData.tradeWon += 1;
+
+          // net_pnl= payout - fee --> trade won
           newData.net_pnl = add(
             accumulator.net_pnl,
             subtract(currentValue.payout, currentValue.totalFee)
           );
         } else {
+          // net_pnl= 0 - fee --> trade lost
           newData.net_pnl = add(
             accumulator.net_pnl,
             subtract('0', currentValue.totalFee)
@@ -76,7 +96,13 @@ export const useProfileGraphQl = () => {
         newData.volume = add(accumulator.volume, currentValue.totalFee);
         return newData;
       },
-      { totalPayout: '0', tradeWon: 0, volume: '0', net_pnl: '0' }
+      {
+        totalPayout: '0',
+        tradeWon: 0,
+        volume: '0',
+        net_pnl: '0',
+        tradesPerAsset: {},
+      } as metricsData
     );
 
     //counts openInterest
