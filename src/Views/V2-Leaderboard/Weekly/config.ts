0caf1ce3a33f3db092dc444b9b783540da78c443
Faraday26 <faraday@buffer.finance>
2023-02-27 12:51:17 +0530

    fix not url history not working on leaderboard

--- src/Views/V2-Leaderboard/Weekly/config.ts
+++ src/Views/V2-Leaderboard/Weekly/config.ts
@@ -1,11 +1,8 @@
-type startTimestampObj = { [key: number]: number | undefined };
-type endTimestampObj = { [key: number]: number | undefined | null };
+export type startTimestampObj = { [key: number]: number };
+export type endTimestampObj = { [key: number]: number };
 
 export const startTimestamp: startTimestampObj = {
   421613: 1676908800000,
   42161: 1676908800000,
 };
-export const endDay: endTimestampObj = {
-  421613: null,
-  42161: null,
-};
+export const endDay: endTimestampObj = {};
