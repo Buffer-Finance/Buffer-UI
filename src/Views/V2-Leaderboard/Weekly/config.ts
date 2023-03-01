a17c191849daa53b8214e92ba893eba6747143aa
Faraday26 <faraday@buffer.finance>
2023-03-01 19:23:12 +0530

    end weekly leaderboard

--- src/Views/V2-Leaderboard/Weekly/config.ts
+++ src/Views/V2-Leaderboard/Weekly/config.ts
@@ -5,4 +5,6 @@ export const startTimestamp: startTimestampObj = {
   421613: 1676908800000,
   42161: 1676908800000,
 };
-export const endDay: endTimestampObj = {};
+export const endDay: endTimestampObj = {
+  421613: 0,
+};
