ab7ff41d6cf20e3ba50b621c0465ac5a2fee9b05
maxwell-1999 <maxwell@buffer.finance>
2023-03-10 14:12:23 +0530

    WIP

--- src/Test.tsx
+++ src/Test.tsx
@@ -52,7 +52,7 @@ var json = {
         children: [
           {
             type: 'tabset',
-            weight: 65,
+            weight: 60,
             selected: 0,
             id: 'charts',
             children: [
