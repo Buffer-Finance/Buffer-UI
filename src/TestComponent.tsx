4d00506f0d6f0be0a75268ed8c8a378d88cbc152
maxwell-1999 <maxwell@buffer.finance>
2023-03-14 15:04:44 +0530

    WIP

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -3,7 +3,7 @@ const TestComponent: React.FC<any> = ({}) => {
     <div>
       <iframe
         style={{ width: '100vw', height: '100vh' }}
-        src="https://testnet.buffer.finance"
+        src="https://testnet-buffer-finance-qwnqxo9nj-bufferfinance.vercel.app/#/binary/BTC-USD?bg-2=161618&bg-1=29292c&bg-signature=ffb313&hide=true&bg-0=111111"
       ></iframe>
     </div>
   );
--- src/Utils/getHashUrlQueryParam.ts
+++ src/Utils/getHashUrlQueryParam.ts
@@ -2,6 +2,7 @@ const getHashUrlQueryParam = (url: string) => {
   if (!url.includes('#')) return;
   const [hash, query] = url.split('#')[1].split('?');
   const params = Object.fromEntries(new URLSearchParams(query));
+  console.log(`params: `, params);
   return params;
 };
 
