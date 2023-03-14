45e5f28490685b5b0645853f77f08b5852675223
maxwell-1999 <maxwell@buffer.finance>
2023-03-14 15:12:59 +0530

    WIP

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -3,7 +3,7 @@ const TestComponent: React.FC<any> = ({}) => {
     <div>
       <iframe
         style={{ width: '100vw', height: '100vh' }}
-        src="https://testnet-buffer-finance-qwnqxo9nj-bufferfinance.vercel.app/#/binary/BTC-USD?bg-2=161618&bg-1=29292c&bg-signature=ffb313&hide=true&bg-0=111111"
+        src="https://testnet-buffer-finance-git-iframe-tut-bufferfinance.vercel.app/#/binary/BTC-USD?bg-2=161618&bg-1=29292c&bg-signature=ffb313&hide=true&bg-0=111111&img=app.level.finance/assets/logo-4d8fd6f5.svg"
       ></iframe>
     </div>
   );
