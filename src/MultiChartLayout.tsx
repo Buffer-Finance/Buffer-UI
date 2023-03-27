db93a86878e10f306d57d6c5172e4bf78763f048
maxwell-1999 <maxwell@buffer.finance>
2023-03-27 21:04:00 +0530

    idle time updated to half an hour

--- src/MultiChartLayout.tsx
+++ src/MultiChartLayout.tsx
@@ -482,7 +482,7 @@ const DesktopTrad = () => {
   //   arr.buttons.push(<Tab></Tab>)
   // }}
 };
-const DesktopTrade = WithIdle(DesktopTrad, 10 * 60 * 1000);
+const DesktopTrade = WithIdle(DesktopTrad, 30 * 60 * 1000);
 export const priceAtom = atom<Partial<Market2Prices>>({});
 export { DesktopTrade };
 
