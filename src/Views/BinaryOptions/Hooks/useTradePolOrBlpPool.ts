3b8e0ecde830bb8a91c191458ab2d98d67b14f55
faraday <faraday@buffer.finance>
2023-04-14 17:18:31 +0530

    feed options contract based on userinput

--- src/Views/BinaryOptions/Hooks/useTradePolOrBlpPool.ts
+++ src/Views/BinaryOptions/Hooks/useTradePolOrBlpPool.ts
@@ -1,9 +1,14 @@
+import { useAtomValue } from 'jotai';
 import { useQTinfo } from '..';
 import { useActivePoolObj } from '../PGDrawer/PoolDropDown';
+import { ammountAtom } from '../PGDrawer';
+import { lt } from '@Utils/NumString/stringArithmatics';
 
 export const useTradePolOrBlpPool = () => {
   const { activePair } = useQTinfo();
   const { activePoolObj } = useActivePoolObj();
+  const userInput = useAtomValue(ammountAtom);
+  console.log(userInput, 'userInput');
   let response = {
     min_amount: activePoolObj.token.min_amount,
     option_contract: activePoolObj.options_contracts,
@@ -11,10 +16,16 @@ export const useTradePolOrBlpPool = () => {
   const polExists = activePair.pools.find((pool) =>
     pool.token.name.toLowerCase().includes('pol')
   );
-  if (polExists)
-    response = {
-      min_amount: polExists.token.min_amount,
-      option_contract: polExists.options_contracts,
-    };
+  if (polExists) {
+    response.min_amount = polExists.token.min_amount;
+    if (
+      userInput &&
+      userInput !== '' &&
+      userInput < activePoolObj.token.min_amount
+    ) {
+      response.option_contract = polExists.options_contracts;
+    }
+  }
+
   return response;
 };
