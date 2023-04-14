5d0cb1ac76e4fec556babed3524f50ff6cc1e008
faraday <faraday@buffer.finance>
2023-04-14 19:38:16 +0530

    update message

--- src/Views/Common/TokenDataNotIncludedWarning.tsx
+++ src/Views/Common/TokenDataNotIncludedWarning.tsx
@@ -17,8 +17,8 @@ export const TokenDataNotIncludedWarning = ({
             alt="lightning"
             className="mr-3 mt-2 h-[18px]"
           />
-          Trades under 100 USDC are not accounted for currently, stats for the
-          same will be live shortly.
+          Stats for trades under 100 USDC are currently not being displayed.
+          This will be fixed shortly.
         </>
       }
       className="!mb-3"
