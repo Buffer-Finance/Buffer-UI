477cbc32855f2dbe731ed686566e628e750e40e9
faraday <faraday@buffer.finance>
2023-04-24 13:44:39 +0530

    fix graph value

--- src/Views/Profile/LBFR/Hooks/useGraphql.ts
+++ src/Views/Profile/LBFR/Hooks/useGraphql.ts
@@ -17,20 +17,21 @@ export const getWeekIdFromTimestamp = (timestamp: number) => {
 export const useLBFRGraphql = () => {
   const { configContracts } = useActiveChain();
   const { address: account } = useAccount();
+  const currentWeekId = getWeekId(0);
+  const lastWeekId = getWeekId(1);
 
   const { data } = useSWR(`LBFR-graphql-query-account-${account}`, {
     fetcher: async () => {
       const response = await axios.post(configContracts.graph.MAIN, {
         query: `{ 
-          totalVolume: lbfrstatsPerUsers(where: {userAddress: "${account}", period: weekly, periodID: "${getWeekId(
-          0
-        )}"}) {
+          totalVolume: lbfrstatsPerUsers(where: {userAddress: "${account}", period: weekly, periodID_in: ["${currentWeekId}","${lastWeekId}"]}) {
                 volume
                 volumeUSDC
                 volumeARB
                 claimable
                 claimed
                 currentSlab
+                periodID
               }
               lbfrclaimDataPerUser(id: "${account}"){
                 lastClaimedTimestamp
@@ -45,6 +46,7 @@ export const useLBFRGraphql = () => {
           claimed: string;
           claimable: string;
           currentSlab: string;
+          periodID: string;
         }[];
         lbfrclaimDataPerUser: {
           lastClaimedTimestamp: string;
@@ -55,7 +57,36 @@ export const useLBFRGraphql = () => {
   });
 
   // console.log(data, 'data');
-  return data;
+  const totalVolume = useMemo(() => {
+    if (data?.totalVolume) {
+      const { totalVolume } = data;
+      const totalVolumeObj = totalVolume.reduce(
+        (acc, curr) => {
+          acc.volume = add(acc.volume, curr.volume);
+          acc.volumeUSDC = add(acc.volumeUSDC, curr.volumeUSDC);
+          acc.volumeARB = add(acc.volumeARB, curr.volumeARB);
+          acc.claimed = add(acc.claimed, curr.claimed);
+          acc.claimable = add(acc.claimable, curr.claimable);
+          if (curr.periodID === currentWeekId.toString()) {
+            acc.currentSlab = add(acc.currentSlab, curr.currentSlab);
+          }
+          return acc;
+        },
+        {
+          volume: '0',
+          volumeUSDC: '0',
+          volumeARB: '0',
+          claimed: '0',
+          claimable: '0',
+          currentSlab: '0',
+        }
+      );
+      return [totalVolumeObj];
+    }
+    return null;
+  }, [data]);
+
+  return { totalVolume, lbfrclaimDataPerUser: data?.lbfrclaimDataPerUser };
 };
 
 export type LBFRGraphqlType =
