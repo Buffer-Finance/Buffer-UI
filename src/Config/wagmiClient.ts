e16617b5d6f346d7dbed45cf7d8396971dbf966d
maxwell-1999 <maxwell@buffer.finance>
2023-04-25 13:24:04 +0530

    Fix

--- src/Config/wagmiClient.ts
+++ src/Config/wagmiClient.ts
@@ -77,7 +77,7 @@ const { chains, provider } = configureChains(getChains(), [publicProvider()]);
 const connectors = connectorsForWallets(getWallets(chains));
 export { chains };
 const wagmiClient = createClient({
-  // autoConnect: true,
+  autoConnect: true,
   connectors,
   provider,
 });
