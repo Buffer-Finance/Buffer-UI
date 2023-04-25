84844d28c191388bcbc0957d11a42bd8f2588d2f
maxwell-1999 <maxwell@buffer.finance>
2023-04-25 17:18:01 +0530

    WIP

--- src/Hooks/UsdcTransfer.tsx
+++ src/Hooks/UsdcTransfer.tsx
@@ -2,15 +2,80 @@ import { encodeMulti } from 'ethers-multisend';
 import TestAvatarAbi from '@Views/TestAvatarAbi.json';
 import { useWriteCall } from './useWriteCall';
 import { useReadCall } from '@Utils/useReadCall';
+import { ethers } from 'ethers';
+const ifc = new ethers.utils.Interface(TestAvatarAbi);
+const AABI = [
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'unacceptedAddress',
+        type: 'address',
+      },
+    ],
+    name: 'NotAuthorized',
+    type: 'error',
+  },
+  {
+    inputs: [
+      { internalType: 'address payable', name: 'to', type: 'address' },
+      { internalType: 'uint256', name: 'value', type: 'uint256' },
+      { internalType: 'bytes', name: 'data', type: 'bytes' },
+    ],
+    name: 'exec',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      { internalType: 'address payable', name: 'to', type: 'address' },
+      { internalType: 'uint256', name: 'value', type: 'uint256' },
+      { internalType: 'bytes', name: 'data', type: 'bytes' },
+      { internalType: 'uint8', name: 'operation', type: 'uint8' },
+    ],
+    name: 'execTransactionFromModule',
+    outputs: [{ internalType: 'bool', name: 'success', type: 'bool' }],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'module',
+    outputs: [{ internalType: 'address', name: '', type: 'address' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      { internalType: 'address', name: '', type: 'address' },
+      { internalType: 'address', name: '', type: 'address' },
+      { internalType: 'uint256', name: '', type: 'uint256' },
+      { internalType: 'bytes', name: '', type: 'bytes' },
+    ],
+    name: 'onERC721Received',
+    outputs: [{ internalType: 'bytes4', name: '', type: 'bytes4' }],
+    stateMutability: 'pure',
+    type: 'function',
+  },
+  {
+    inputs: [{ internalType: 'address', name: '_module', type: 'address' }],
+    name: 'setModule',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  { stateMutability: 'payable', type: 'receive' },
+];
 
 const UsdcTransfer: React.FC<any> = ({}) => {
   const { writeCall } = useWriteCall(
-    '0xE69334B9Ca6409fDbe590efC2f16A847D12B673B',
-    TestAvatarAbi
+    '0x99c758a4Aff8d0d51F18c5fBd94fD182ec49BaaA',
+    AABI
   );
   const calls = [
     {
-      address: '0xE69334B9Ca6409fDbe590efC2f16A847D12B673B',
+      address: '0x28346dd04079a2b48daF1AC933Fe8F04D92bc773',
       abi: TestAvatarAbi,
       name: 'balanceOf',
       params: ['0xFbEA9559AE33214a080c03c68EcF1D3AF0f58A7D'],
@@ -24,14 +89,20 @@ const UsdcTransfer: React.FC<any> = ({}) => {
     const encoded = encodeMulti(
       [
         {
-          to: '0xB66127377ff3618b595177b5E84f8ee9827CD061',
-          value: '10000000',
-          data: '0x00',
+          to: '0x28346dd04079a2b48daF1AC933Fe8F04D92bc773',
+          value: '0',
+          data: ifc.encodeFunctionData('transfer', [
+            '0xB66127377ff3618b595177b5E84f8ee9827CD061',
+            '10000000',
+          ]),
         },
         {
-          to: '0x4E6f6E255246f2392A15662d03396aBF08890866',
-          value: '10000000',
-          data: '0x00',
+          to: '0x28346dd04079a2b48daF1AC933Fe8F04D92bc773',
+          value: '0',
+          data: ifc.encodeFunctionData('transfer', [
+            '0x4E6f6E255246f2392A15662d03396aBF08890866',
+            '10000000',
+          ]),
         },
       ],
       '0xAb3224e76fa5a46D9f8364cd14F4cB03087d6Fd8'
