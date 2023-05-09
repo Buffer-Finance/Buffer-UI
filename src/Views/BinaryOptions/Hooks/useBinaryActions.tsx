5552b648dab525ebabff0f0b292aa9ea099bf4f1
maxwell-1999 <maxwell@buffer.finance>
2023-05-09 12:19:50 +0530

    WIP

--- src/Views/BinaryOptions/Hooks/useBinaryActions.tsx
+++ src/Views/BinaryOptions/Hooks/useBinaryActions.tsx
@@ -13,7 +13,7 @@ import {
   multiply,
 } from '@Utils/NumString/stringArithmatics';
 import { sessionAtom } from 'src/atoms/generic';
-import { getPriceFromKlines } from 'src/TradingView/useDataFeed';
+import { getPriceFromKlines, sleep } from 'src/TradingView/useDataFeed';
 import { approveModalAtom, QuickTradeExpiry } from '../PGDrawer';
 import { getUserError, timeToMins } from '../PGDrawer/TimeSelector';
 import { slippageAtom } from '../Components/SlippageModal';
@@ -27,10 +27,15 @@ import { priceAtom } from '@Hooks/usePrice';
 import { knowTillAtom } from './useIsMerketOpen';
 import { getDisplayDate, getDisplayTime } from '@Utils/Dates/displayDateTime';
 import { useTradePolOrBlpPool } from './useTradePolOrBlpPool';
+import { isTestnet } from 'config';
+import axios from 'axios';
+import { useAccount } from 'wagmi';
+import { useActiveChain } from '@Hooks/useActiveChain';
 
 export const useBinaryActions = (userInput, isYes, isQuickTrade = false) => {
   const binary = useQTinfo();
   const [settings] = useAtom(slippageAtom);
+  const { configContracts } = useActiveChain();
   const referralData = useReferralCode(binary.activeChain);
   const activeAssetState = useActiveAssetState(userInput, referralData);
   const [balance, allowance, _, currStats] = activeAssetState;
@@ -39,7 +44,9 @@ export const useBinaryActions = (userInput, isYes, isQuickTrade = false) => {
   const { highestTierNFT } = useHighestTierNFT({ userOnly: true });
   const [, setIsApproveModalOpen] = useAtom(approveModalAtom);
   const { state, dispatch } = useGlobal();
+  console.log(`useBinaryActions-state: `, state);
   const activeAsset = binary.activePair;
+  const { address } = useAccount();
   const { activePoolObj } = useActivePoolObj();
   const { writeCall } = useWriteCall(binary.routerContract, routerABI);
   const { writeCall: approve } = useWriteCall(
@@ -184,14 +191,12 @@ export const useBinaryActions = (userInput, isYes, isQuickTrade = false) => {
       customTrade.is_up,
       option_contract.current,
       toFixed(multiply(('' + price).toString(), 8), 0),
-      // '10000000000',
       toFixed(multiply(settings.slippage.toString(), 2), 0),
       settings.allowPartial,
       referralData[2],
       highestTierNFT?.tokenId || '0',
     ];
-
-    writeCall(cb, 'initiateTrade', args, null, {
+    const confirmationModal = {
       content: (
         <div className="nowrap flex">
           Position opened for&nbsp;
@@ -201,7 +206,71 @@ export const useBinaryActions = (userInput, isYes, isQuickTrade = false) => {
           {/* @ <Display data={priceData?.close} label="$" /> */}
         </div>
       ),
-    });
+    };
+    if (isTestnet) {
+      const id = 'InstantTradingId';
+      const user_signature = 'sign';
+      const signature_timestamp = 'sing-ts';
+      const instantTradingApiUrl =
+        'https://app.buffer.finance/api/v1/instant-trading';
+      const TradeQuery = {
+        totalFee: args[0],
+        period: args[1],
+        isAbove: args[2],
+        targetContract: args[3],
+        expectedStrike: args[4],
+        slippage: args[5],
+        allowPartialFill: args[6],
+        referralCode: args[7],
+        traderNFTId: args[8],
+      };
+      const reqBody = {
+        trade: TradeQuery,
+        pair: activeAsset.tv_id,
+        user_address: address,
+        user_signature,
+        signature_timestamp,
+        env: configContracts.env,
+      };
+      // dispatch({ type: 'SET_TXN_LOADING', payload: 1 });
+      toastify({
+        id,
+        msg: 'Transaction confirmation in progress...',
+        type: 'info',
+        inf: 1,
+      });
+      try {
+        await sleep(2000);
+        const txHash = 'fsadfasd';
+        console.log(`useBinaryActions-reqBody: `, reqBody);
+        // dispatch({ type: 'SET_TXN_LOADING', payload: 0 });
+        toastify({
+          id,
+          msg: confirmationModal.content,
+          type: 'success',
+          hash: `${binary.activeChain.blockExplorers?.default.url}/tx/${txHash}`,
+          body: null,
+          confirmationModal: null,
+          timings: 100,
+        });
+      } catch (e) {
+        toastify({
+          id,
+          msg: (
+            <span>
+              Oops! There is some error. Can you please try again?
+              <br />
+              <span className="!text-3">Error: {JSON.stringify(e)}</span>
+            </span>
+          ),
+          type: 'error',
+        });
+      }
+      setLoading(null);
+      // const response = await axios.post(instantTradingApiUrl, reqBody);
+    } else {
+      writeCall(cb, 'initiateTrade', args, null, confirmationModal);
+    }
   };
 
   const handleApproveClick = async (ammount = toFixed(getPosInf(), 0)) => {
