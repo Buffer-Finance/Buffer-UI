104206f1f078328da5e65ca6f0e0c16b9d68ae9c
faraday <faraday@buffer.finance>
2023-05-19 10:37:47 +0530

    add the graph query

--- src/Views/BinaryOptions/V3/useV3Config.ts
+++ src/Views/BinaryOptions/V3/useV3Config.ts
@@ -3,12 +3,6 @@ import axios from 'axios';
 import { useMemo } from 'react';
 import useSWR from 'swr';
 import { v3Config } from './v3Config';
-import configABI from '../ABI/configABI.json';
-import { convertBNtoString, useSignerOrPorvider } from '@Utils/useReadCall';
-import { multicallv2 } from '@Utils/Contract/multiContract';
-import getDeepCopy from '@Utils/getDeepCopy';
-import { MarketInterface } from 'src/MultiChart';
-const Calls = ['minPeriod', 'maxPeriod', 'minFee', 'maxFee'];
 
 export const useNoLossStaticConfig = () => {
   const { activeChain } = useActiveChain();
@@ -24,107 +18,108 @@ export const useNoLossStaticConfig = () => {
   return data;
 };
 
-const marketid2Info = {
-  BTCUSD: {
-    price_precision: 100,
-    pair: 'BTC-USD',
-    category: 'Crypto',
-    fullName: 'Bitcoin',
-    tv_id: 'BTCUSD',
-  },
-  ETHUSD: {
-    price_precision: 100,
-    pair: 'ETH-USD',
-    category: 'Crypto',
-    fullName: 'Ethereum',
-    tv_id: 'ETHUSD',
-  },
-  GBPUSD: {
-    price_precision: 100,
-    pair: 'GBP-USD',
-    category: 'Forex',
-    fullName: 'Pound',
-    tv_id: 'GBPUSD',
-  },
-  EURUSD: {
-    price_precision: 100,
-    pair: 'EUR-USD',
-    category: 'Forex',
-    fullName: 'Euro',
-    tv_id: 'EURUSD',
-  },
-};
-
 const useV3Config = () => {
   const config = useNoLossStaticConfig();
-  const sOrP = useSignerOrPorvider();
-  console.log('goes inhere');
-  return useSWR<{ [a: string]: MarketInterface }>(
-    `config-root-config-${config.chainId}`,
-    {
-      fetcher: async (name) => {
-        const basicQuery = `
-      optionContracts: optionContracts(
-        first: 1000
-        ) {
-          id
-          address
-          asset
-          isPaused
-        }
-        `;
-        const response = await axios.post(config.graph.MAIN, {
-          query: `{${basicQuery}}`,
-        });
-        console.log(`useV3Config-response: `, response);
-        let calls = [];
 
-        response.data.data.optionContracts.forEach((s) => {
-          Calls.forEach((c) => {
-            calls.push({
-              address: s.config,
-              abi: configABI,
-              name: c,
-              params: [],
-            });
-          });
-        });
-
-        console.log(`useV3Config-calls: `, calls);
-        let returnData = await multicallv2(calls, sOrP, config.multicall, '');
-        let copy = getDeepCopy(returnData);
-        convertBNtoString(copy);
-        let appConfig = {};
-
-        response.data.data.optionContracts.forEach((s, sid) => {
-          const key = s.asset;
-          Calls.forEach((c, cid) => {
-            if (appConfig?.[key]) {
-              appConfig[key] = {
-                ...appConfig[key],
-                [c]: copy[sid + cid][0] + '',
-              };
-            } else {
-              appConfig[key] = {
-                [c]: copy[sid + cid][0] + '',
-              };
-            }
-          });
-          appConfig[key] = {
-            ...appConfig[key],
-            configContract: s.config,
-            isPaused: s.isPaused,
-            optionsContract: s.address,
-            ...marketid2Info[s.asset],
-          };
-        });
-
-        console.log(`appConfig: `, appConfig);
-        return appConfig;
-      },
-      refreshInterval: 100000,
+  async function fetcher() {
+    const basicQuery = `
+  optionContracts(first: 1000) {
+    configContract {
+      address
+      maxPeriod
+      maxFee
+      minFee
+      minPeriod
+      baseSettlementFeeForAbove
+      baseSettlementFeeForBelow
     }
-  );
+    address
+    asset
+    isPaused
+    currentUtilization
+    token
+    pool
+    tradeCount
+    volume
+    payoutForUp
+    payoutForDown
+    openUp
+    openInterest
+    openDown
+  }
+  `;
+    const response = await axios.post(config.graph.MAIN, {
+      query: `{${basicQuery}}`,
+    });
+    return response.data.data.optionContracts.reduce(
+      (
+        acc: {
+          [key: string]: V3ConfigInterface & V3OptionInterface & addedConfig;
+        },
+
+        option: V3ConfigResponse
+      ) => {
+        //update the pool list if an asset comes more tahn once
+        if (acc[option.asset]) {
+          acc[option.asset].pools.push(option.pool);
+        } else {
+          acc[option.asset] = {
+            ...option.configContract,
+            configAddress: option.configContract?.address,
+            ...option,
+            pools: [option.pool],
+          };
+        }
+
+        return acc;
+      },
+      {}
+    );
+  }
+
+  return useSWR<Promise<V3Config>>(`config-root-config-${config.chainId}`, {
+    fetcher,
+    refreshInterval: 100000,
+  });
 };
 
 export { useV3Config };
+
+export interface V3ConfigInterface {
+  address: string;
+  maxPeriod: string;
+  maxFee: string;
+  minFee: string;
+  minPeriod: string;
+  baseSettlementFeeForAbove: string;
+  baseSettlementFeeForBelow: string;
+}
+
+export interface V3OptionInterface {
+  address: string;
+  asset: string;
+  isPaused: boolean;
+  currentUtilization: string;
+  token: string;
+  pool: string;
+  tradeCount: number;
+  volume: string;
+  payoutForUp: string;
+  payoutForDown: string;
+  openUp: string;
+  openInterest: string;
+  openDown: string;
+}
+
+export interface V3Config {
+  [key: string]: V3ConfigInterface & V3OptionInterface & addedConfig;
+}
+
+interface addedConfig {
+  configAddress: string;
+  pools: string[];
+}
+
+type V3ConfigResponse = {
+  configContract: V3ConfigInterface;
+} & V3OptionInterface;
