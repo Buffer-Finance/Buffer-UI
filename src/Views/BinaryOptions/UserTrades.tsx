912079796a0143dd36f8b6a85e6c3a95cf8779f2
maxwell-1999 <maxwell@buffer.finance>
2023-05-23 11:51:36 +0530

    WIP

--- src/Views/BinaryOptions/UserTrades.tsx
+++ src/Views/BinaryOptions/UserTrades.tsx
@@ -196,7 +196,9 @@ export const UserTrade: React.FC<{
     <div className={userTradeRootDivStyle}>
       <div className="flex items-center justify-between text-1 text-f12">
         <TradeMarket trade={trade} />
-
+        <div className="flex-col">
+          {timeTillExpiration} left<div>Ony shown in dev </div>
+        </div>
         <NumberTooltip content={timeTillExpiration + ' lerft'}>
           <div className="flex items-center gap-x-[4px]">
             <TimerIcon />
