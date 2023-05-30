5f0df4c97ff49af414aec41dc1b9bfda239b6ce6
maxwell-1999 <maxwell@buffer.finance>
2023-05-30 13:05:08 +0530

    WIP

---
+++ src/locales/en/messages.ts
@@ -0,0 +1,1 @@
+/*eslint-disable*/export const messages=JSON.parse("{\"J/hVSQ\":[[\"0\"]],\"BuijXr\":[\"<0>\",[\"0\"],\"</0>\"],\"UDNFrt\":\"Contests\",\"xGVfLh\":\"Continue\",\"qnLna/\":\"Continue with <0/>\",\"WfCysZ\":\"dello\"}");
