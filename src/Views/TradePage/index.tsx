994f7dccdc7f72e03a38582c43ec11aca07d46ac
Faraday26 <faraday@buffer.finance>
2023-06-06 20:28:22 +0530

    resolve conflict

--- src/Views/TradePage/index.tsx
+++ src/Views/TradePage/index.tsx
@@ -1,22 +1,24 @@
-import { Trans } from '@lingui/macro';
 import { MarketChart } from './Views/MarketChart';
 import { AccordionTable } from './Views/AccordionTable';
 import { OneCTModal } from '@Views/OneCT/OneCTModal';
 import { BuyTrade } from './Views/BuyTrade';
+import { AssetSelectorDD } from './Views/Markets/AssetSelectorDD';
+import { PinnedMarkets } from './Views/Markets/PinnedMarkets';
 
 const TradePage: React.FC<any> = ({}) => {
   return (
     <>
       <EssentialModals />
-      <div className="flex   justify-between w-[100%]">
+      <div className="flex justify-between w-[100%]">
         <div className="flex flex-col w-full">
+          {/* <PinnedMarkets /> */}
+          {/* <AssetSelectorDD /> */}
           <MarketChart />
           <AccordionTable />
         </div>
         <div className="h-[100%]">
           <BuyTrade />
-        </div>{' '}
-        {/* <Trans>hello</Trans> */}
+        </div>
       </div>
     </>
   );
