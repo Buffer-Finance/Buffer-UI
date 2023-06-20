8221ffddff7a346c39d85625fd6da38cee264e94
Faraday26 <faraday@buffer.finance>
2023-06-20 14:08:55 +0530

    add roundoff in numbers

--- src/Views/TradePage/Hooks/useCurrentPrice.ts
+++ src/Views/TradePage/Hooks/useCurrentPrice.ts
@@ -1,6 +1,7 @@
 import { priceAtom } from '@Hooks/usePrice';
 import { getPriceFromKlines } from '@TV/useDataFeed';
 import { toFixed } from '@Utils/NumString';
+import { roundToTwo } from '@Utils/roundOff';
 import { marketsForChart } from '@Views/TradePage/config';
 import { joinStrings } from '@Views/TradePage/utils';
 import { useAtomValue } from 'jotai';
@@ -18,5 +19,6 @@ export const useCurrentPrice = ({
     marketsForChart[marketId as keyof typeof marketsForChart];
   const price = getPriceFromKlines(marketPrice, activeChartMarket);
   const precision = activeChartMarket.price_precision.toString().length - 1;
-  return { currentPrice: toFixed(price, precision), precision };
+  // console.log(`precision: `, precision, price);
+  return { currentPrice: Number(roundToTwo(price, precision)), precision };
 };
