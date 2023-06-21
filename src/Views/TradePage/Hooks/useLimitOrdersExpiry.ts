245b857f40387b7d8b0a55a0f0fabb47f8642277
Faraday26 <faraday@buffer.finance>
2023-06-21 12:47:54 +0530

    fix

--- src/Views/TradePage/Hooks/useLimitOrdersExpiry.ts
+++ src/Views/TradePage/Hooks/useLimitOrdersExpiry.ts
@@ -1,19 +1,31 @@
 import { useAtomValue } from 'jotai';
 import { tradeSettingsAtom } from '../atoms';
 import { useMemo } from 'react';
+import { defaultSettings } from '@Views/TradePage/config';
+import { multiply } from '@Utils/NumString/stringArithmatics';
 
 export const useLimitOrdersExpiry = () => {
   const tradeSettings = useAtomValue(tradeSettingsAtom);
 
-  const limitOrderExpiryInMinutes = useMemo(() => {
+  const limitOrderExpiryInSeconds = useMemo(() => {
+    if (
+      tradeSettings.limitOrdersExpiry === '' ||
+      tradeSettings.limitOrdersExpiry === '0' ||
+      tradeSettings.limitOrdersExpiry === undefined
+    )
+      return multiply(defaultSettings.trade.limitOrdersExpiry, '60');
     if (tradeSettings.selectedTimeFrame === 'm') {
-      return tradeSettings.limitOrdersExpiry * 60;
+      return multiply(tradeSettings.limitOrdersExpiry, '60');
     }
     if (tradeSettings.selectedTimeFrame === 'h') {
-      return tradeSettings.limitOrdersExpiry * 60 * 60;
+      return multiply(tradeSettings.limitOrdersExpiry, '3600');
     }
   }, [tradeSettings.limitOrdersExpiry, tradeSettings.selectedTimeFrame]);
 
-  console.log('limitOrderExpiryInMinutes', limitOrderExpiryInMinutes);
-  return limitOrderExpiryInMinutes;
+  // console.log(
+  //   'limitOrderExpiryInSeconds',
+  //   limitOrderExpiryInSeconds,
+  //   tradeSettings.limitOrdersExpiry
+  // );
+  return limitOrderExpiryInSeconds;
 };
