14f7c479364ca856138a26c0c3a236f6fbd7a24f
maxwell-1999 <maxwell@buffer.finance>
2023-06-23 16:43:14 +0530

    WIP

--- src/Views/TradePage/Hooks/usePriceChange.ts
+++ src/Views/TradePage/Hooks/usePriceChange.ts
@@ -1,17 +1,19 @@
 import useSWR from 'swr';
 import { marketsForChart, pricePublisherBaseUrl } from '../config';
 import axios from 'axios';
+import { PriceQuerySchema } from 'src/Interfaces/interfaces';
 // 25 - 41
 const usePriceChange = () => {
-  const { data } = useSWR('24h-change', {
+  const { data, error } = useSWR('24h-change', {
     fetcher: async () => {
       const nowEpoch = Math.round(Date.now() / 1000) - 3;
       const yesterdayEpoch = nowEpoch - 60 * 60 * 24;
-      const queries = [];
+      let queries: PriceQuerySchema[] = [];
       for (const marketName in marketsForChart) {
         const market = (marketsForChart as any)[marketName];
-        queries.push(
-          axios.post(pricePublisherBaseUrl, [
+        queries = [
+          ...queries,
+          ...[
             {
               pair: market.tv_id,
               timestamp: nowEpoch,
@@ -20,27 +22,40 @@ const usePriceChange = () => {
               pair: market.tv_id,
               timestamp: yesterdayEpoch,
             },
-          ])
-        );
+          ],
+        ];
       }
-      console.time('for-fetching-change');
-      const response = await Promise.all(queries);
-      console.timeEnd('for-fetching-change');
+      const response = await axios.post(pricePublisherBaseUrl, queries);
       const finalResponse: {
         [key: string]: {
           now: number;
           yesterday: number;
-          change: number;
+          change?: number;
         };
       } = {};
-      response.forEach((res) => {
-        finalResponse[res.data[0].pair] = {
-          now: res.data[0].price,
-          yesterday: res.data[1].price,
-          change:
-            ((res.data[0].price - res.data[1].price) / res.data[1].price) * 100,
-        };
+
+      console.log(`response: `, response);
+      response.data.forEach((res) => {
+        if (!finalResponse?.[res.pair]) {
+          finalResponse[res.pair] = {};
+        }
+        if (res.timestamp == nowEpoch) {
+          finalResponse[res.pair].now = res.price;
+        } else {
+          finalResponse[res.pair].yesterday = res.price;
+        }
       });
+      console.log(`finalResponse: `, finalResponse);
+      for (const key in finalResponse) {
+        if (finalResponse[key].now && finalResponse[key].yesterday) {
+          finalResponse[key].change =
+            ((finalResponse[key].now - finalResponse[key].yesterday) /
+              finalResponse[key].yesterday) *
+            100;
+        }
+      }
+      console.log(`finalResponse: `, finalResponse);
+
       return finalResponse;
     },
     refreshInterval: 1000 * 60 * 5,
@@ -51,6 +66,7 @@ const usePriceChange = () => {
   // for each market, add 2 req, one of now, one of now - 24h.
 
   // when response arrived, calculate the change
+  console.log(`error: `, error);
 
   return data;
 };
