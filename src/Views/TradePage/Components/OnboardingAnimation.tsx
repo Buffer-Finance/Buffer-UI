f3aee83bd0a158c039aaaf3b105c2cda89e06c25
maxwell-1999 <maxwell@buffer.finance>
2023-07-04 12:42:11 +0530

    WIP

--- src/Views/TradePage/Components/OnboardingAnimation.tsx
+++ src/Views/TradePage/Components/OnboardingAnimation.tsx
@@ -5,10 +5,10 @@ import { useEffect, SVGProps } from 'react';
 import BufferLogo from '@Assets/Elements/BufferLogo';
 import BufferAnimation from '@SVG/BufferAnimation';
 
-const duration = 5000;
+const duration = 500000;
 // 37
 const OnboardingAnimation: React.FC<any> = ({}) => {
-  const isOpen = useAtomValue(showOnboardingAnimationAtom);
+  const isOpen = true;
 
   return (
     <Dialog
@@ -32,7 +32,7 @@ const ModalChild = () => {
   });
 
   return (
-    <div className="flex w-[100vw] h-[100vh] overflow-hidden items-center  justify-center gap-x-[150px] mr-[-80px]">
+    <div className="flex w-[100vw] h-[100vh] overflow-hidden items-center  justify-center gap-x-[150px] mt-[-3vh]">
       <div className="text-1 font-[700]">
         <div className="text-[85px]">Welcome to</div>
         <div className="text-[150px] mt-[-44px]">Buffer</div>
