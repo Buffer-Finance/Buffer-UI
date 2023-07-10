9f44537a55fc3ebc011e8b32793bbcb35feeab76
maxwell-1999 <124770082+maxwell-1999@users.noreply.github.com>
2023-07-10 12:02:59 +0530

    Update usePrice.ts

--- src/Hooks/usePrice.ts
+++ src/Hooks/usePrice.ts
@@ -1,8 +1,9 @@
 import { UTF8ArrToStr, getKlineFromPrice } from '@TV/utils';
 import axios from 'axios';
-import { atom, useSetAtom } from 'jotai';
-import { useEffect } from 'react';
+import { atom, useAtomValue, useSetAtom } from 'jotai';
+import { useEffect, useState } from 'react';
 import { Market2Prices } from 'src/Types/Market';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 
 import {
   PythConnection,
@@ -12,13 +13,32 @@ import { Connection } from '@solana/web3.js';
 import { multiply } from '@Utils/NumString/stringArithmatics';
 import Big from 'big.js';
 import useSWR from 'swr';
-
+type WSUPdate = {
+  type: 'price_update';
+  price_feed: {
+    ema_price: {
+      conf: string;
+      expo: number;
+      price: string;
+      publish_time: number;
+    };
+    id: string;
+    price: {
+      conf: string;
+      expo: number;
+      price: string;
+      publish_time: number;
+    };
+  };
+};
 const solanaClusterName = 'pythnet';
 const solanaWeb3Connection = 'https://pythnet.rpcpool.com/';
 
 export const usePrice = (fetchInitialPrices?: boolean) => {
   const setPrice = useSetAtom(priceAtom);
-
+  const prices = useAtomValue(priceAtom);
+  const { sendMessage, lastJsonMessage, lastMessage, readyState } =
+    useWebSocket('wss://xc-mainnet.pyth.network/ws');
   const subscribeToStreamUpdates = async () => {
     const url = 'https://pyth-api.vintage-orange-muffin.com/v2/streaming';
     const response = await fetch(url);
@@ -38,6 +58,7 @@ export const usePrice = (fetchInitialPrices?: boolean) => {
       }
     }
   };
+  const [messageHistory, setMessageHistory] = useState([]);
 
   const subscribeToWSUpdates = async () => {
     const pythConnection = new PythConnection(
@@ -74,17 +95,56 @@ export const usePrice = (fetchInitialPrices?: boolean) => {
     });
     pythConnection.start();
   };
-
   useEffect(() => {
-    const interval = setInterval(async () => {
-      const data = await getPrice();
+    console.log(`lastMessage: `, lastMessage, lastJsonMessage);
+    if (!lastJsonMessage) return;
+    if ((lastJsonMessage as WSUPdate).type == 'price_update') {
+      const data = {
+        [pythIds[(lastJsonMessage as WSUPdate).price_feed.id]]: [
+          {
+            price: multiply(
+              (lastJsonMessage as WSUPdate).price_feed.price.price,
+              new Big('10')
+                .pow((lastJsonMessage as WSUPdate).price_feed.price.expo)
+                .toString()
+            ),
+            time:
+              (lastJsonMessage as WSUPdate).price_feed.price.publish_time *
+              1000,
+          },
+        ],
+      };
+      console.log(`ws-update-data: `, data);
+
       setPrice((p) => ({ ...p, ...data }));
-    }, 1000);
-    return () => {
-      clearInterval(interval);
+    }
+  }, [lastMessage, lastJsonMessage, setMessageHistory]);
+  useEffect(() => {
+    if (!sendMessage) return;
+    const obj = {
+      ids: [
+        'ff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace',
+        'e62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43',
+        '84c2dde9633d93d1bcad84e7dc41c9d56578b7ec52fabedc1f335d673df0a7c1',
+        'a995d00bb36a63cef7fd2c287dc105fc8f3d93779f062f09551b0af3e81ec30b',
+        '3fa4252848f9f0a1480be62745a4629d9eb1322aebab8a791e344b3b9c1adcf5',
+        '2f95862b045670cd22bee3114c39763a4a08beeb663b145d283c31d7d1101c4f',
+        '5de33a9112c2b700b8d30b8a3402c103578ccfa2765696471cc672bd5cf6ac52',
+      ],
+      type: 'subscribe',
     };
-    // subscribeToWSUpdates();
-  }, []);
+    const resp = sendMessage(JSON.stringify(obj));
+  }, [sendMessage]);
+  // useEffect(() => {
+  //   const interval = setInterval(async () => {
+  //     const data = await getPrice();
+  //     setPrice((p) => ({ ...p, ...data }));
+  //   }, 1000);
+  //   return () => {
+  //     clearInterval(interval);
+  //   };
+  // }, []);
+  console.log('prices', prices);
 };
 
 export const wsStateAtom = atom<{ state: string }>({
@@ -105,9 +165,9 @@ export const pythIds = {
 export const getPrice = async () => {
   const price = await axios.get(
     `https://xc-mainnet.pyth.network/api/latest_price_feeds?` +
-    Object.keys(pythIds)
-      .map((d) => 'ids[]=0x' + d)
-      .join('&')
+      Object.keys(pythIds)
+        .map((d) => 'ids[]=0x' + d)
+        .join('&')
   );
   const marketPrice = {};
   price.data.forEach((e) => {
