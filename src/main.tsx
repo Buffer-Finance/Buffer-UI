8c40f0b5fb87954b8608efca325deb612bdf8950
maxwell-1999 <maxwell@buffer.finance>
2023-07-19 11:53:13 +0530

    WIP

--- src/main.tsx
+++ src/main.tsx
@@ -1,39 +1,10 @@
-import axios from 'axios';
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import { WagmiConfig } from 'wagmi';
-import App from './App';
-import '@rainbow-me/rainbowkit/styles.css'
-import '@szhsin/react-menu/dist/index.css';
-import '@szhsin/react-menu/dist/theme-dark.css';
-import '@szhsin/react-menu/dist/transitions/slide.css';
 
-import wagmiClient, { chains } from './Config/wagmiClient';
-import ContextProvider from './contexts';
-import { SWRConfig } from 'swr';
-import { Provider as JotaiProvider } from 'jotai';
-import { RainbowKitProvider,darkTheme } from '@rainbow-me/rainbowkit';
-
-const options = {
-  fetcher: (url: string) =>
-    axios.get('https://api-v2.buffer.finance/' + url).then((res) => res.data),
-  refreshInterval: 1000,
-};
 import { inject } from '@vercel/analytics';
 inject();
+console.log('deb-envproductionenv', import.meta.env.Production);
+console.log('deb-envpreviewenv', import.meta.env.Preview);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains} theme={darkTheme()}>
-        <HashRouter>
-          <SWRConfig value={options}>
-            <ContextProvider>
-              <JotaiProvider>
-                <App />
-              </JotaiProvider>
-            </ContextProvider>
-          </SWRConfig>
-        </HashRouter>
-      </RainbowKitProvider>
-    </WagmiConfig>
+  <div>Hye i am main branch</div>
 );
