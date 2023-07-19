267b211d806fd4322b5f12976a069988f4e7f9f9
maxwell-1999 <maxwell@buffer.finance>
2023-07-19 12:14:33 +0530

    WIP

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import ReactDOM from 'react-dom/client';
 
 import { inject } from '@vercel/analytics';
 inject();
-console.log('deb-envproductionenv', import.meta.env.Production);
-console.log('deb-envpreviewenv', import.meta.env.Preview);
+console.log('deb-env import.meta.env.VITE_PROD', import.meta.env.VITE_PROD);
+console.log('deb-env import.meta.env.VITE_DEV', import.meta.env.VITE_DEV);
+console.log('deb-env import.meta.env.VITE_PRE', import.meta.env.VITE_PRE);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <div>Hye i am main branch</div>
