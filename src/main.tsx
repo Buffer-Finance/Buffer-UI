3c7411675abff9fc4917320501a20fc6f59df914
maxwell-1999 <maxwell@buffer.finance>
2023-07-19 12:19:05 +0530

    Added envs

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ inject();
 console.log('deb-env import.meta.env.VITE_PROD', import.meta.env.VITE_PROD);
 console.log('deb-env import.meta.env.VITE_DEV', import.meta.env.VITE_DEV);
 console.log('deb-env import.meta.env.VITE_PRE', import.meta.env.VITE_PRE);
+console.log('deb-env import.meta.env.VITE_CHECK', import.meta.env.VITE_CHECK);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <div>Hye i am main branch</div>
