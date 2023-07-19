c4467405e8941de54885c72528ec7a7df2592a3b
maxwell-1999 <maxwell@buffer.finance>
2023-07-19 12:56:29 +0530

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -55,10 +55,7 @@ if (import.meta.env.VITE_MODE === 'production') {
 
 import { inject } from '@vercel/analytics';
 inject();
-console.log(
-  'deb-env import.meta.env.VITE_PROD',
-  import.meta?.env?.VITE_PREVIEW_ONLY
-);
+console.log('deb-env', import.meta.env.VITE_PREVIEW_ONLY);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Sentry.ErrorBoundary fallback={<ErrorComponenet />}>
