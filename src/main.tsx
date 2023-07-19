83c4adcb2481ef60613d79e0be3d89391984d9b5
maxwell-1999 <maxwell@buffer.finance>
2023-07-19 12:24:43 +0530

    WIP

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { inject } from '@vercel/analytics';
 inject();
 console.log('deb-env import.meta.env.VITE_PROD', import.meta?.env.VITE_PROD);
-console.log('deb-env import.meta.env.VITE_DEV', import.meta?.env.VITE_DEV);
-console.log('deb-env import.meta.env.VITE_PRE', import.meta?.env.VITE_PRE);
 console.log('deb-env import.meta.env.VITE_CHECK', import.meta?.env.VITE_CHECK);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
