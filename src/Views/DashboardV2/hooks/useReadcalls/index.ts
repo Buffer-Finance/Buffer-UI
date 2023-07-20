249d0138b215c5d2fb5f1f60df50aa6f32030d1e
Faraday26 <faraday@buffer.finance>
2023-07-20 19:41:37 +0530

    fix infinite loading

--- src/Views/DashboardV2/hooks/useReadcalls/index.ts
+++ src/Views/DashboardV2/hooks/useReadcalls/index.ts
@@ -1,13 +1,23 @@
 import { useCall2Data } from '@Utils/useReadCall';
-import { readCallsAtom, readResponseAtom } from '@Views/DashboardV2/atoms';
+import {
+  readCallsAtom,
+  readResponseAtom,
+  setReadCallsAtom,
+} from '@Views/DashboardV2/atoms';
 import { useAtomValue, useSetAtom } from 'jotai';
+import { useEffect } from 'react';
 
 export const useReadcalls = () => {
   const readcalls = useAtomValue(readCallsAtom);
   const setResponse = useSetAtom(readResponseAtom);
+  const setCalls = useSetAtom(setReadCallsAtom);
 
-  // console.log('readcalls', readcalls);
-
+  useEffect(() => {
+    setCalls({ readcalls: [], isCleanup: true });
+    return () => {
+      setCalls({ readcalls: [], isCleanup: true });
+    };
+  }, []);
   const { data, error } = useCall2Data(readcalls, 'dashboardV2-readcalls');
   if (error) console.log(error);
   setResponse(data);
