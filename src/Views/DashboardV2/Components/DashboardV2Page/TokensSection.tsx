5341755ae1eea0d72a2b668cf00742a804ef30ff
Faraday26 <faraday@buffer.finance>
2023-07-20 14:22:43 +0530

    fix

--- src/Views/DashboardV2/Components/DashboardV2Page/TokensSection.tsx
+++ src/Views/DashboardV2/Components/DashboardV2Page/TokensSection.tsx
@@ -5,6 +5,7 @@ import { BLPcard } from '../Cards/BLPcard';
 import { useUBLPdata } from '@Views/DashboardV2/hooks/useReadcalls/useUBLPdata';
 import { useABLPdata } from '@Views/DashboardV2/hooks/useReadcalls/useABLPdata';
 import { ChainNotSupported } from '@Views/Common/ChainNotSupported';
+import { arbitrum } from 'wagmi/chains';
 
 const TokensSection = () => {
   return (
@@ -17,7 +18,7 @@ const TokensSection = () => {
         <BFRcard />,
         <UBLPcard />,
         <ChainNotSupported
-          supportedChainIds={[]}
+          supportedChainIds={[arbitrum.id]}
           children={<ABLPcard />}
           hide
         />,
