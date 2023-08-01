5e1f75a784dd5bf26187cbf36745eabca87790f3
faraday <faraday@buffer.finance>
2023-08-01 15:06:02 +0530

    add arb token data

--- src/Views/DashboardV2/Components/DashboardV2Page/TokensSection.tsx
+++ src/Views/DashboardV2/Components/DashboardV2Page/TokensSection.tsx
@@ -5,7 +5,7 @@ import { BLPcard } from '../Cards/BLPcard';
 import { useUBLPdata } from '@Views/DashboardV2/hooks/useReadcalls/useUBLPdata';
 import { useABLPdata } from '@Views/DashboardV2/hooks/useReadcalls/useABLPdata';
 import { ChainNotSupported } from '@Views/Common/ChainNotSupported';
-import { arbitrum } from 'wagmi/chains';
+import { arbitrum, arbitrumGoerli } from 'wagmi/chains';
 
 const TokensSection = () => {
   return (
@@ -18,7 +18,7 @@ const TokensSection = () => {
         <BFRcard />,
         <UBLPcard />,
         <ChainNotSupported
-          supportedChainIds={[arbitrum.id]}
+          supportedChainIds={[arbitrum.id, arbitrumGoerli.id]}
           children={<ABLPcard />}
           hide
         />,
