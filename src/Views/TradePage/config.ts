8579e111d13fc82c673a05ed1fbf51a6c6884039
faraday <faraday@buffer.finance>
2023-08-09 14:20:50 +0530

    change base url

--- src/Views/TradePage/config.ts
+++ src/Views/TradePage/config.ts
@@ -472,7 +472,7 @@ export const marketsForChart = {
   },
 };
 
-export const baseUrl = import.meta.env.VITE_INSTANT_TRADING_HOST_SANDBOX;
+export const baseUrl = import.meta.env.VITE_INSTANT_TRADING_HOST_DEVELOPMENT;
 export const pricePublisherBaseUrl = import.meta.env.VITE_PRICE_QUERY_HOST;
 
 export type earnConfigType = keyof (typeof appConfig)['42161']['EarnConfig'];
