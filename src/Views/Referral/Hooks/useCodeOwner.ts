268f8dc893caa70e81a5eb3082c8ed75c77c9920
maxwell-1999 <maxwell@buffer.finance>
2023-08-10 14:39:01 +0530

    WIP

--- src/Views/Referral/Hooks/useCodeOwner.ts
+++ src/Views/Referral/Hooks/useCodeOwner.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import ReferralABI from '@Views/Referral/Config/ReferralABI.json';
 import useDebouncedEffect from '@Hooks/Utilities/useDeboncedEffect';
 import { usePublicClient } from 'wagmi';
@@ -7,13 +7,21 @@ import { useActiveChain } from '@Hooks/useActiveChain';
 import { getConfig } from '@Views/TradePage/utils/getConfig';
 import { getContract } from 'viem';
 
+import { createPublicClient, http } from 'viem';
+import { mainnet } from 'viem/chains';
+
 export function useCodeOwner(code: string) {
   const [owner, setOwner] = useState(null);
   const { activeChain } = useActiveChain();
   const provider = usePublicClient({ chainId: activeChain.id });
   const configContracts = getConfig(activeChain.id);
   const referralAddress = configContracts.referral_storage;
-
+  const publicClient = useMemo(() => {
+    return createPublicClient({
+      chain: activeChain,
+      transport: http(),
+    });
+  }, [activeChain]);
   const referralContract = getContract({
     address: referralAddress,
     abi: ReferralABI,
@@ -22,7 +30,12 @@ export function useCodeOwner(code: string) {
 
   const updateOwner = async () => {
     if (!code) return;
-    const tempOwner = await contractRead(referralContract, 'codeOwner', [code]);
+    const tempOwner = await publicClient.readContract({
+      address: referralAddress,
+      abi: ReferralABI,
+      functionName: 'codeOwner',
+      args: [code],
+    });
     setOwner(tempOwner);
   };
   useDebouncedEffect(
