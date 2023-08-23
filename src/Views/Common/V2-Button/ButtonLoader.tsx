f0dbfe035a3dd5baabc44801317fba4f32ed63ac
maxwell-1999 <maxwell@buffer.finance>
2023-08-23 20:45:31 +0530

    WIP

--- src/Views/Common/V2-Button/ButtonLoader.tsx
+++ src/Views/Common/V2-Button/ButtonLoader.tsx
@@ -4,7 +4,10 @@ interface IButtonLoader {
 
 const ButtonLoader: React.FC<IButtonLoader> = ({ className }) => {
   return (
-    <div className={`snippet ${className}`} data-title=".dot-carousel">
+    <div
+      className={`snippet b1200:h-[20px] ${className}`}
+      data-title=".dot-carousel"
+    >
       <div className="stage">
         <div className="dot-carousel"></div>
       </div>
