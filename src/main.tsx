d66fff046b243f6a3ae236892908104ac1c30979
faraday <faraday@buffer.finance>
2023-09-01 10:50:54 +0530

    remove sentry consoles

--- src/main.tsx
+++ src/main.tsx
@@ -29,17 +29,8 @@ const options = {
     axios.get('https://api-v2.buffer.finance/' + url).then((res) => res.data),
   refreshInterval: 1000,
 };
-console.log(
-  import.meta.env.VITE_SENTRY_DSN,
-  import.meta.env.VITE_MODE,
-  'sentry'
-);
+
 if (import.meta.env.VITE_MODE === 'production') {
-  console.log(
-    'sentry works',
-    import.meta.env.VITE_SENTRY_DSN,
-    import.meta.env.VITE_MODE
-  );
   Sentry.init({
     dsn: import.meta.env.VITE_SENTRY_DSN,
 
