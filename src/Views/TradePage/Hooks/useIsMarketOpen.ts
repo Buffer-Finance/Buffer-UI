3b82de379804e566326341e0a5e55ea51907f43c
faraday <faraday@buffer.finance>
2023-09-02 10:39:04 +0530

    fix buy buttons

--- src/Views/TradePage/Hooks/useIsMarketOpen.ts
+++ src/Views/TradePage/Hooks/useIsMarketOpen.ts
@@ -5,8 +5,10 @@ import { getAddress } from 'viem';
 
 export const useIsMarketOpen = (
   market: marketType | undefined,
-  selectedPoolContract: string | undefined
+  selectedPoolContract: string | undefined,
+  selectedMarketContract: string | undefined
 ) => {
+  // console.log('useIsMarketOpen', market, selectedMarketContract || '');
   const readcallData = useBuyTradeData();
   const isForex =
     market?.category === AssetCategory[AssetCategory.Forex] ||
@@ -15,15 +17,18 @@ export const useIsMarketOpen = (
   const isOpen = useMemo(() => {
     if (!readcallData || !market) return false;
     const currentPool = market.pools.find((pool) => {
-      if (
-        isForex &&
-        !readcallData.creationWindows[getAddress(pool.optionContract)]
-      )
-        return false;
       return pool.pool === selectedPoolContract;
     });
-    return !currentPool?.isPaused;
-  }, [selectedPoolContract, market, readcallData]);
+    if (!isForex && currentPool?.isPaused) return false;
+
+    if (
+      isForex &&
+      selectedMarketContract &&
+      !readcallData.creationWindows[getAddress(selectedMarketContract)]
+    )
+      return false;
+    return true;
+  }, [selectedPoolContract, market, readcallData, selectedMarketContract]);
 
   return { isMarketOpen: isOpen, isForex };
 };
