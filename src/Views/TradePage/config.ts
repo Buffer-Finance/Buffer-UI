9009be5547e0c94c7a1df88223101c1192e2baba
Faraday <faraday@buffer.finance>
2023-09-04 14:00:48 +0530

    first frame 3m

--- src/Views/TradePage/config.ts
+++ src/Views/TradePage/config.ts
@@ -231,16 +231,16 @@ export const defaultSettings = {
 export const defaultSelectedTime = '00:15';
 export const oneSec = 1000;
 export const durations = [
-  {
-    duration: 1 * 60,
-    time: '00:01',
-    name: ['1m'],
-  },
   // {
-  //   duration: 3 * 60 * oneSec,
-  //   time: '00:03',
-  //   name: ['3m'],
+  //   duration: 1 * 60,
+  //   time: '00:01',
+  //   name: ['1m'],
   // },
+  {
+    duration: 3 * 60 ,
+    time: '00:03',
+    name: ['3m'],
+  },
   {
     duration: 5 * 60,
     time: '00:05',
