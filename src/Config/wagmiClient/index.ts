ca9d31c060bad38e8963ed5e7ec1b4822be4cf49
Faraday <faraday@buffer.finance>
2023-09-04 18:33:33 +0530

    remove polygon chain

--- src/Config/wagmiClient/index.ts
+++ src/Config/wagmiClient/index.ts
@@ -1,10 +1,10 @@
-import { configureChains, createConfig } from 'wagmi';
-import { publicProvider } from 'wagmi/providers/public';
-import { connectorsForWallets } from '@rainbow-me/rainbowkit';
 import { getHashUrlQueryParam } from '@Utils/getHashUrlQueryParam';
 import { inIframe } from '@Utils/isInIframe';
-import { getWallets } from './getWallets';
+import { connectorsForWallets } from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
 import { getAllChains, getSupportedChains } from './getConfigChains';
+import { getWallets } from './getWallets';
 import { mockConnector } from './mockConnector';
 export const urlSettings = getHashUrlQueryParam(window.location.href);
 
@@ -18,7 +18,7 @@ const { chains, publicClient } = configureChains(getAllChains(), [
 
 const connectors = isTestEnv
   ? mockConnector
-  : connectorsForWallets(getWallets(getAllChains()));
+  : connectorsForWallets(getWallets(getSupportedChains()));
 
 const wagmiClient = createConfig({
   autoConnect: inIframe() ? false : true,
