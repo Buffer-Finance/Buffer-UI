231b422f91b6d2f606c7726d6ddce08f4bf05eb6
Faraday <faraday@buffer.finance>
2023-09-11 11:18:30 +0530

    fix 24h change api

--- src/Views/TradePage/Hooks/usePriceChange.ts
+++ src/Views/TradePage/Hooks/usePriceChange.ts
@@ -1,36 +1,18 @@
-import useSWR from 'swr';
-import { marketsForChart, pricePublisherBaseUrl } from '../config';
 import axios from 'axios';
+import useSWR from 'swr';
+import { pricePublisherBaseUrl } from '../config';
+import { useMarketsConfig } from './useMarketsConfig';
 // 25 - 41
 const usePriceChange = () => {
+  const markets = useMarketsConfig();
   const { data } = useSWR('24h-change', {
     fetcher: async () => {
-      // const nowEpoch = Math.round(Date.now() / 1000) - 5 * 60;
-      // const yesterdayEpoch = nowEpoch - 60 * 60 * 24;
-      const queries = [];
-      for (const marketName in marketsForChart) {
-        const market = (marketsForChart as any)[marketName];
-        // queries.push(
-        //   axios.post(pricePublisherBaseUrl, [
-        //     {
-        //       pair: market.tv_id,
-        //       timestamp: nowEpoch,
-        //     },
-        //     {
-        //       pair: market.tv_id,
-        //       timestamp: yesterdayEpoch,
-        //     },
-        //   ])
-        // );
+      if (!markets) return;
 
-        // queries.push(
-        //   axios.get(
-        //     pricePublisherBaseUrl + '24h_change/' + `?pair=${market.tv_id}`
-        //   )
-        // );
+      const queries: string[] = [];
 
-        queries.push(market.tv_id);
-      }
+      markets?.forEach((market) => queries.push(market.tv_id));
+
       const { data: response } = await axios.post(
         pricePublisherBaseUrl + 'bulk_24h_change/',
         queries
@@ -44,8 +26,6 @@ const usePriceChange = () => {
 
       response.forEach((res: { pair: string; change: number }) => {
         finalResponse[res.pair] = {
-          // now: res.data[0].price,
-          // yesterday: res.data[1].price,
           pair: res.pair,
           change: res.change,
         };
@@ -55,12 +35,6 @@ const usePriceChange = () => {
     refreshInterval: 1000 * 60 * 5,
   });
 
-  // get markets from config
-
-  // for each market, add 2 req, one of now, one of now - 24h.
-
-  // when response arrived, calculate the change
-  // console.log(data, 'usePriceCHanfedata');
   return data;
 };
 
