6a34d2fb1986d4518ee3091d65f5d02933691566
Faraday <faraday@buffer.finance>
2023-09-14 11:48:05 +0530

    fix settings menu

--- src/Views/Common/Navbar/SettingsDD.tsx
+++ src/Views/Common/Navbar/SettingsDD.tsx
@@ -1,25 +1,45 @@
 import { Settings } from '@Views/TradePage/Views/Settings';
-import { ClickEvent, MenuItem } from '@szhsin/react-menu';
-import { ReactMenu } from '../ReactMenu';
+import {
+  ClickEvent,
+  ControlledMenu,
+  MenuItem,
+  useClick,
+  useMenuState,
+} from '@szhsin/react-menu';
+import { useRef } from 'react';
 import { SettingsIcon } from './SettingsIcon';
 
 export const SettingsDD: React.FC = () => {
-  // const ref = useRef(null);
-  // const [menuState, toggleMenu] = useMenuState({ transition: true });
-  // const anchorProps = useClick(menuState.state, toggleMenu);
+  const ref = useRef(null);
+  const [menuState, toggleMenu] = useMenuState({ transition: true });
+  const anchorProps = useClick(menuState.state, toggleMenu);
 
-  // function closeDropdown() {
-  //   toggleMenu(false);
-  // }
+  function closeDropdown() {
+    toggleMenu(false);
+  }
 
-  function closeDropdown() {}
   return (
-    <ReactMenu
-      MenuClassName={'!p-[0] !rounded-[10px] hover:!rounded-[10px]'}
-      MenuHeader={
+    <>
+      <button
+        type="button"
+        ref={ref}
+        {...anchorProps}
+        test-id="setting-button-click"
+      >
         <SettingsIcon className="hover:brightness-125 transition-all duration-100 ease-in-out" />
-      }
-      MenuOptions={
+      </button>
+      <ControlledMenu
+        {...menuState}
+        anchorRef={ref}
+        onClose={closeDropdown}
+        viewScroll="initial"
+        direction="bottom"
+        position="anchor"
+        align="end"
+        portal
+        menuClassName={'!p-[0] !rounded-[10px] hover:!rounded-[10px]'}
+        offsetY={10}
+      >
         <MenuItem
           className={({ hover }) => {
             return `!p-[0] ${hover ? '!rounded-[10px]' : ''}`;
@@ -30,43 +50,7 @@ export const SettingsDD: React.FC = () => {
         >
           <Settings closeDropdown={closeDropdown} />
         </MenuItem>
-      }
-    />
+      </ControlledMenu>
+    </>
   );
-
-  // return (
-  //   <>
-  //     <button
-  //       type="button"
-  //       ref={ref}
-  //       {...anchorProps}
-  //       test-id="setting-button-click"
-  //     >
-  //       <SettingsIcon className="hover:brightness-125 transition-all duration-100 ease-in-out" />
-  //     </button>
-  //     <ControlledMenu
-  //       {...menuState}
-  //       anchorRef={ref}
-  //       onClose={closeDropdown}
-  //       viewScroll="initial"
-  //       direction="bottom"
-  //       position="anchor"
-  //       align="end"
-  //       portal
-  //       menuClassName={'!p-[0] !rounded-[10px] hover:!rounded-[10px]'}
-  //       offsetY={10}
-  //     >
-  //       <MenuItem
-  //         className={({ hover }) => {
-  //           return `!p-[0] ${hover ? '!rounded-[10px]' : ''}`;
-  //         }}
-  //         onClick={(e: ClickEvent) => {
-  //           e.keepOpen = true;
-  //         }}
-  //       >
-  //         <Settings closeDropdown={closeDropdown} />
-  //       </MenuItem>
-  //     </ControlledMenu>
-  //   </>
-  // );
 };
