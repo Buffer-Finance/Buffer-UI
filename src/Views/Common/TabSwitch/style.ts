0693b302fc5c40d312d82abad3d9f9d2d1147479
Faraday <faraday@buffer.finance>
2023-10-05 12:07:32 +0530

    fix profile mobile tables

--- src/Views/Common/TabSwitch/style.ts
+++ src/Views/Common/TabSwitch/style.ts
@@ -1,9 +1,15 @@
-import styled from "styled-components";
+import styled from '@emotion/styled';
 
 const Background = styled.a`
+  & ::-webkit-scrollbar {
+    background: var(--bg-grey);
+    height: 2px !important;
+    width: 3px !important;
+  }
   .tab-pannel {
     margin-top: 1rem;
     transition: 200ms;
+    overflow: auto;
   }
   .tab-pannel--enter {
     transform: translate(-100px, 0);
