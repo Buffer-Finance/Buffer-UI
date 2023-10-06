ee9a3766817ecac4374668658d4196df69641c72
Faraday <faraday@buffer.finance>
2023-10-06 17:10:22 +0530

    handle redirection to profile in non platform tables

--- src/Views/TradePage/Views/AccordionTable/CancelTable.tsx
+++ src/Views/TradePage/Views/AccordionTable/CancelTable.tsx
@@ -1,11 +1,13 @@
 import FailureIcon from '@SVG/Elements/FailureIcon';
 import { divide } from '@Utils/NumString/stringArithmatics';
+import { getSlicedUserAddress } from '@Utils/getUserAddress';
 import BufferTable from '@Views/Common/BufferTable';
 import NumberTooltip from '@Views/Common/Tooltips';
 import { Display } from '@Views/Common/Tooltips/Display';
 import { buyTradeDataAtom } from '@Views/TradePage/Hooks/useBuyTradeData';
 import { usePoolInfo } from '@Views/TradePage/Hooks/usePoolInfo';
 import { TradeType } from '@Views/TradePage/type';
+import { Launch } from '@mui/icons-material';
 import { useAtomValue } from 'jotai';
 import { useMedia } from 'react-use';
 import { getAddress } from 'viem';
@@ -16,12 +18,13 @@ import {
   TableErrorRow,
   TableHeader,
 } from './Common';
+import { useNavigateToProfile } from './HistoryTable';
 
 export const CancelledTable: React.FC<{
   trades: TradeType[] | undefined;
   onlyView?: number[];
-  activePage?: number;
-  setActivePage?: (page: number) => void;
+  activePage: number;
+  setActivePage: (page: number) => void;
   totalPages?: number;
   platform?: boolean;
   isLoading: boolean;
@@ -40,6 +43,8 @@ export const CancelledTable: React.FC<{
 }) => {
   const { getPoolInfo } = usePoolInfo();
   const isMobile = useMedia('(max-width:600px)');
+  const isNotMobile = useMedia('(min-width:1200px)');
+  const navigateToProfile = useNavigateToProfile();
   const readcallData = useAtomValue(buyTradeDataAtom);
 
   let strikePriceHeading = 'Strike Price';
@@ -48,15 +53,26 @@ export const CancelledTable: React.FC<{
     strikePriceHeading = 'Strike';
     tradeSizeHeading = 'Size';
   }
-  const headNameArray = [
-    'Asset',
-    strikePriceHeading,
-    tradeSizeHeading,
-    'Queue',
-    'Cancellation',
-    'Reason',
-    'Status',
-  ];
+  const headNameArray = platform
+    ? [
+        'Asset',
+        strikePriceHeading,
+        tradeSizeHeading,
+        'Queue',
+        'Cancellation',
+        'Reason',
+        'Status',
+        'User',
+      ]
+    : [
+        'Asset',
+        strikePriceHeading,
+        tradeSizeHeading,
+        'Queue',
+        'Cancellation',
+        'Reason',
+        'Status',
+      ];
 
   enum TableColumn {
     Asset = 0,
@@ -66,6 +82,7 @@ export const CancelledTable: React.FC<{
     CancellationTime = 4,
     Reason = 5,
     Status = 6,
+    User = 7,
   }
   const HeaderFomatter = (col: number) => {
     return <TableHeader col={col} headsArr={headNameArray} />;
@@ -82,6 +99,25 @@ export const CancelledTable: React.FC<{
       readcallData.currentOIs[getAddress(trade.target_contract)];
 
     switch (col) {
+      case TableColumn.User:
+        if (platform)
+          return (
+            <div className="flex items-center">
+              {getSlicedUserAddress(trade.user_address, 4)}
+              {!isNotMobile && (
+                <div
+                  role="button"
+                  onClick={() => {
+                    const userAddress = trade.user_address;
+                    if (!userAddress) return;
+                    navigateToProfile(userAddress);
+                  }}
+                >
+                  <Launch className="scale-75 mb-1" />
+                </div>
+              )}
+            </div>
+          );
       case TableColumn.Strike:
         return (
           <StrikePriceComponent
@@ -155,7 +191,13 @@ export const CancelledTable: React.FC<{
       cols={headNameArray.length}
       rows={trades ? trades.length : 0}
       widths={['auto']}
-      onRowClick={console.log}
+      onRowClick={(idx) => {
+        if (isNotMobile && platform) {
+          const userAddress = trades?.[idx].user_address;
+          if (!userAddress) return;
+          navigateToProfile(userAddress);
+        }
+      }}
       showOnly={onlyView}
       overflow={overflow}
       error={<TableErrorRow msg="No active trades present." />}
