8b65bdc8b1771699e684f52905068c0c0de993ff
maxwell-1999 <maxwell@buffer.finance>
2023-10-09 18:50:07 +0530

    WIP Fix

--- src/Views/TradePage/utils/getMinimumValue.ts
+++ src/Views/TradePage/utils/getMinimumValue.ts
@@ -5,8 +5,13 @@ export default function getMinimumValue(
   value1: string,
   value2: string
 ): string {
-  if (gt(value1, value2)) {
-    return value2;
+  try {
+    if (gt(value1, value2)) {
+      return value2;
+    }
+    return value1;
+  } catch (e) {
+    console.log('stats-deb', value1, value2, typeof value1, typeof value2);
+    return '0';
   }
-  return value1;
 }
