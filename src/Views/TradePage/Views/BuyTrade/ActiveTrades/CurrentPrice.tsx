04cec79192ab36a8f5cebd73bafb4a05079b5a03
Faraday <faraday@buffer.finance>
2023-10-18 14:42:19 +0530

    add skeleton for price

--- src/Views/TradePage/Views/BuyTrade/ActiveTrades/CurrentPrice.tsx
+++ src/Views/TradePage/Views/BuyTrade/ActiveTrades/CurrentPrice.tsx
@@ -1,5 +1,6 @@
 import { Display } from '@Views/Common/Tooltips/Display';
 import { useCurrentPrice } from '@Views/TradePage/Hooks/useCurrentPrice';
+import { Skeleton } from '@mui/material';
 
 export const CurrentPrice: React.FC<{
   token0: string;
@@ -9,6 +10,8 @@ export const CurrentPrice: React.FC<{
     token0,
     token1,
   });
+  if (currentPrice === 0) return <Skeleton className="w-[100px] !h-7 lc " />;
+
   return (
     <Display
       className="!justify-start"
