5805f8a2ceb1fd9579f2bad32502b7e8865647ca
Faraday <faraday@buffer.finance>
2023-10-26 12:56:52 +0530

    remove onect modal

--- src/Views/Common/Navbar/index.tsx
+++ src/Views/Common/Navbar/index.tsx
@@ -1,26 +1,22 @@
-import { useMemo, useState } from 'react';
-import { BufferLogoComponent } from './BufferLogo';
-import { getTabs } from 'src/Config/getTabs';
-import { TabsDropdown } from './TabsDropDown';
-import { Tab } from './Tab';
-import { AccountDropdown } from './AccountDropdown';
 import { useGlobal } from '@Contexts/Global';
-import { useAtomValue } from 'jotai';
-import { urlSettings } from 'src/Config/wagmiClient';
-import { isTestnet } from 'config';
-import { SettingsDD } from './SettingsDD';
-import { activeMarketFromStorageAtom } from 'src/globalStore';
-import MemoWalletSVG from '@SVG/Elements/WalletSVG';
 import MemoHamburgerSVG from '@SVG/Elements/HamburgerSVG2';
-import { useShutterHandlers } from '../MobileShutter/MobileShutter';
-import { useOngoingTrades } from '@Views/TradePage/Hooks/useOngoingTrades';
+import MemoWalletSVG from '@SVG/Elements/WalletSVG';
 import { OneCTModal } from '@Views/OneCT/OneCTModal';
-import { useNavigate } from 'react-router-dom';
-import { inIframe } from '@Utils/isInIframe';
+import { useOngoingTrades } from '@Views/TradePage/Hooks/useOngoingTrades';
+import { isTestnet } from 'config';
+import { useAtomValue } from 'jotai';
+import { useMemo } from 'react';
+import { getTabs } from 'src/Config/getTabs';
+import { urlSettings } from 'src/Config/wagmiClient';
+import { activeMarketFromStorageAtom } from 'src/globalStore';
+import { useShutterHandlers } from '../MobileShutter/MobileShutter';
+import { AccountDropdown } from './AccountDropdown';
+import { BufferLogoComponent } from './BufferLogo';
+import { SettingsDD } from './SettingsDD';
+import { Tab } from './Tab';
+import { TabsDropdown } from './TabsDropDown';
 
-interface INavbar {}
-
-export const Navbar: React.FC<INavbar> = () => {
+export const Navbar: React.FC = () => {
   const { dispatch } = useGlobal();
   const activeMarketFromStorage = useAtomValue(activeMarketFromStorageAtom);
   const tabs = useMemo(
@@ -28,7 +24,6 @@ export const Navbar: React.FC<INavbar> = () => {
     [activeMarketFromStorage]
   );
   const VISIBLETABS = isTestnet ? 6 : 6;
-  // const MORETABS = isTestnet ? 2 : 3;
   const handleClose = () => {
     dispatch({
       type: 'UPDATE_SIDEBAR_STATE',
@@ -36,16 +31,7 @@ export const Navbar: React.FC<INavbar> = () => {
   };
   const { openOngoingTradesShutter, shutterState } = useShutterHandlers();
   const [activeTrades, limitOrderTrades] = useOngoingTrades();
-  const navigate = useNavigate();
-  const [click, setClick] = useState(0);
-  const openAdmin = () => {
-    setClick((click) => {
-      if (click > 5) {
-        navigate('/admin');
-      }
-      return click + 1;
-    });
-  };
+
   const show = !urlSettings?.hide;
   return (
     <header className="  sticky bg-[#232334] top-[0px] flex justify-between w-full h-[45px] pr-[8px] header top-0 z-[102] b1200:z-10">
@@ -93,43 +79,19 @@ export const Navbar: React.FC<INavbar> = () => {
               }
               return <Tab tab={tab} key={tab.name} />;
             })}
-            {/* {tabs.length > VISIBLETABS && (
-              <TabsDropdown
-                tabs={tabs.slice(VISIBLETABS, -MORETABS)}
-                defaultName="Analytics"
-              />
-            )} */}
+
             {tabs.length > VISIBLETABS && (
               <TabsDropdown tabs={tabs.slice(VISIBLETABS)} defaultName="More" />
             )}
-            {/* <TabsDropdown tabs={social} defaultName="Socials" /> */}
           </div>
         )}
       </div>
       <div className="flex items-center gap-[3px] whitespace-nowrap">
-        {inIframe() && (
-          <button
-            onClick={openAdmin}
-            className="bg-transparent w-[100px] h-[35px]"
-          ></button>
-        )}
         <div id="dropdown-box" className="flex gap-4 items-center text-1">
           <AccountDropdown />
         </div>
 
         <SettingsDD />
-
-        {/* <div id="mobile-sidebar-logo" className="a1400:!hidden sm:hidden">
-          {state.sidebar_active ? (
-            <MenuLogo className="icon menu" onClick={handleClose} />
-          ) : (
-            <CloseLogo
-              className="icon menu"
-              onClick={handleClose}
-              style={{ transform: 'scale(0.8)' }}
-            />
-          )}
-        </div> */}
       </div>
     </header>
   );
