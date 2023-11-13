652543df7d689f5f7ccf1c11adb691edb46e1b87
Faraday <faraday@buffer.finance>
2023-11-13 18:57:54 +0530

    fix UI

---
+++ src/Views/NoLoss-V3/Admin/Components/Form/SubmitButton.tsx
@@ -0,0 +1,9 @@
+import { BlueBtn } from '@Views/Common/V2-Button';
+
+export const SubmitButton = () => {
+  return (
+    <BlueBtn onClick={() => {}} className="!w-fit !text-f14 !px-3 !py-1 !h-fit">
+      Create Tournament
+    </BlueBtn>
+  );
+};
--- src/Views/NoLoss-V3/Admin/Components/Stepper.tsx
+++ src/Views/NoLoss-V3/Admin/Components/Stepper.tsx
@@ -1,33 +1,49 @@
-import { StepLabel } from '@mui/material';
+import HorizontalTransition from '@Views/Common/Transitions/Horizontal';
+import { BlueBtn } from '@Views/Common/V2-Button';
+import { StepButton, StepLabel } from '@mui/material';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Step from '@mui/material/Step';
 import Stepper from '@mui/material/Stepper';
 import Typography from '@mui/material/Typography';
 import * as React from 'react';
+import { LeaderboardRules } from './Form/LeaderboardRules';
+import { TournamentConditions } from './Form/TournamentConditions';
+import { TournamentMeta } from './Form/TournamentMeta';
 
-const steps = [
-  'Tournament Meta',
-  'Tournament Conditions',
-  'Leaderboard Conditions',
-];
+const btnClasses = '!w-fit !text-f14 !px-3 !py-1 !h-fit mx-4';
 
-export default function HorizontalNonLinearStepper() {
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [completed, setCompleted] = React.useState<{
-    [k: number]: boolean;
-  }>({});
+export default function HorizontalNonLinearStepper({
+  steps,
+  activeStateIndex,
+  setActiveStep,
+  completed,
+  setCompleted,
+  submitButton,
+  isFormFilled,
+}: {
+  steps: string[];
+  activeStateIndex: number;
+  setActiveStep: (newIndex: number) => void;
+  completed: number[] | null;
+  setCompleted: (newIndex: number | null) => void;
+  submitButton: JSX.Element;
+  isFormFilled: (formIndex: number) => boolean;
+}) {
+  // const [completed, setCompleted] = React.useState<{
+  //   [k: number]: boolean;
+  // }>({});
 
   const totalSteps = () => {
     return steps.length;
   };
 
   const completedSteps = () => {
-    return Object.keys(completed).length;
+    return completed ? completed.length : 0;
   };
 
   const isLastStep = () => {
-    return activeStep === totalSteps() - 1;
+    return activeStateIndex === totalSteps() - 1;
   };
 
   const allStepsCompleted = () => {
@@ -36,16 +52,16 @@ export default function HorizontalNonLinearStepper() {
 
   const handleNext = () => {
     const newActiveStep =
-      isLastStep() && !allStepsCompleted()
+      isLastStep() && !allStepsCompleted() && completed
         ? // It's the last step, but not all steps have been completed,
           // find the first step that has been completed
           steps.findIndex((step, i) => !(i in completed))
-        : activeStep + 1;
+        : activeStateIndex + 1;
     setActiveStep(newActiveStep);
   };
 
   const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    setActiveStep(activeStateIndex - 1);
   };
 
   const handleStep = (step: number) => () => {
@@ -53,76 +69,101 @@ export default function HorizontalNonLinearStepper() {
   };
 
   const handleComplete = () => {
-    const newCompleted = completed;
-    newCompleted[activeStep] = true;
-    setCompleted(newCompleted);
+    // const newCompleted = completed;
+    // newCompleted[activeStateIndex] = true;
+    if (!isFormFilled(activeStateIndex)) return;
+    setCompleted(activeStateIndex);
     handleNext();
   };
 
   const handleReset = () => {
     setActiveStep(0);
-    setCompleted({});
+    setCompleted(null);
   };
 
   return (
-    <Box sx={{ width: '100%' }}>
-      <Stepper nonLinear activeStep={activeStep} alternativeLabel>
+    <>
+      <Stepper
+        nonLinear
+        activeStep={activeStateIndex}
+        alternativeLabel
+        sx={{ fontSize: 14, marginTop: '12px' }}
+      >
         {steps.map((label, index) => (
-          <Step key={label} completed={completed[index]}>
-            {/* <StepButton onClick={handleStep(index)}> */}
-            <StepLabel>{label}</StepLabel>
-            {/* </StepButton> */}
+          <Step
+            key={label}
+            completed={completed ? completed.includes(index) : false}
+            classes={{ root: '!text-f15' }}
+            sx={{
+              '& .MuiSvgIcon-fontSizeMedium': {
+                scale: '175%',
+                marginTop: '8px',
+              },
+            }}
+          >
+            <StepButton onClick={handleStep(index)}>
+              <StepLabel classes={{ label: '!text-1 !text-f15' }}>
+                {label}
+              </StepLabel>
+            </StepButton>
           </Step>
         ))}
       </Stepper>
       <div>
         {allStepsCompleted() ? (
           <React.Fragment>
-            <Typography sx={{ mt: 2, mb: 1 }}>
+            <Typography sx={{ textAlign: 'center', fontSize: 14 }}>
               All steps completed - you&apos;re finished
             </Typography>
-            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-              <Box sx={{ flex: '1 1 auto' }} />
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                pt: 2,
+                justifyContent: 'space-between',
+                mx: 3,
+              }}
+            >
+              {submitButton}
               <Button onClick={handleReset}>Reset</Button>
             </Box>
           </React.Fragment>
         ) : (
           <React.Fragment>
-            <Typography sx={{ mt: 2, mb: 1, py: 1 }}>
-              Step {activeStep + 1}
-            </Typography>
+            <HorizontalTransition value={activeStateIndex}>
+              <TournamentMeta />
+              <TournamentConditions />
+              <LeaderboardRules />
+            </HorizontalTransition>
             <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-              <Button
-                color="inherit"
-                disabled={activeStep === 0}
-                onClick={handleBack}
-                sx={{ mr: 1 }}
-              >
-                Back
-              </Button>
+              {activeStateIndex > 0 && (
+                <BlueBtn onClick={handleBack} className={btnClasses}>
+                  Back
+                </BlueBtn>
+              )}
               <Box sx={{ flex: '1 1 auto' }} />
-              <Button onClick={handleNext} sx={{ mr: 1 }}>
+              <BlueBtn onClick={handleNext} className={btnClasses}>
                 Next
-              </Button>
-              {activeStep !== steps.length &&
-                (completed[activeStep] ? (
+              </BlueBtn>
+              {activeStateIndex !== steps.length &&
+                (completed && completed.includes(activeStateIndex) ? (
                   <Typography
                     variant="caption"
                     sx={{ display: 'inline-block' }}
                   >
-                    Step {activeStep + 1} already completed
+                    Step {activeStateIndex + 1} already completed
                   </Typography>
                 ) : (
-                  <Button onClick={handleComplete}>
+                  <BlueBtn onClick={handleComplete} className={btnClasses}>
                     {completedSteps() === totalSteps() - 1
                       ? 'Finish'
                       : 'Complete Step'}
-                  </Button>
+                  </BlueBtn>
                 ))}
             </Box>
           </React.Fragment>
         )}
       </div>
-    </Box>
+    </>
   );
 }
