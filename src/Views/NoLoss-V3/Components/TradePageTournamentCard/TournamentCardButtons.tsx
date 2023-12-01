93264b233a3a044614cb6fae0ed0fa6a2ea361f5
maxwell-1999 <maxwell@buffer.finance>
2023-12-01 12:32:14 +0530

    WIP

--- src/Views/NoLoss-V3/Components/TradePageTournamentCard/TournamentCardButtons.tsx
+++ src/Views/NoLoss-V3/Components/TradePageTournamentCard/TournamentCardButtons.tsx
@@ -136,7 +136,6 @@ export const TournamentCardButtons: React.FC<{
         divide(allowance, tournament.buyinTokenDecimals)!,
         ticketCost
       );
-      setBtnLoading(true);
 
       let isBufferRouterApproved = readCallResults?.isTradingApproved;
       const allowanceTxn = isAllowed
@@ -190,7 +189,7 @@ export const TournamentCardButtons: React.FC<{
     const maximumparticipantsReached =
       parseInt(tournament.tournamentLeaderboard.userCount) >=
       parseInt(tournament.tournamentConditions.maxParticipants);
-    setBtnLoading(false);
+
     secondButton = (
       <>
         <BufferButton
