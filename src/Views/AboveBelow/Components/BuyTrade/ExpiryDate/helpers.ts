ca99ea1caf701e0025093a552f59f38543545d7e
Faraday <faraday@buffer.finance>
2023-12-05 13:20:25 +0530

    fix opentimestamps

--- src/Views/AboveBelow/Components/BuyTrade/ExpiryDate/helpers.ts
+++ src/Views/AboveBelow/Components/BuyTrade/ExpiryDate/helpers.ts
@@ -6,16 +6,20 @@ export function formatDateShort(timestamp: number) {
 }
 
 export function getTimestamps(date = Date.now()) {
-  // End of Day (8 AM next day)
-  const endOfDay = new Date(date);
-  endOfDay.setUTCHours(8, 0, 0, 0);
-  endOfDay.setUTCDate(endOfDay.getUTCDate() + 1); // Move to the next day
+  // Start of Day (8 PM current day or next day)
+  const startOfDay = new Date(date);
+  startOfDay.setUTCHours(20, 0, 0, 0);
 
-  // End of Week (Friday) at 8 AM
+  // Check if the current time has passed 8 PM
+  if (startOfDay.getTime() <= date) {
+    startOfDay.setUTCDate(startOfDay.getUTCDate() + 1); // Move to the next day
+  }
+
+  // End of Week (Friday) at 8 PM
   const endOfWeek = new Date(date);
   const daysUntilFriday = (5 - endOfWeek.getUTCDay() + 7) % 7; // Calculate days until Friday
   endOfWeek.setUTCDate(endOfWeek.getUTCDate() + daysUntilFriday);
-  endOfWeek.setUTCHours(8, 0, 0, 0);
+  endOfWeek.setUTCHours(20, 0, 0, 0);
 
-  return [endOfDay.getTime(), endOfWeek.getTime()];
+  return [startOfDay.getTime(), endOfWeek.getTime()];
 }
