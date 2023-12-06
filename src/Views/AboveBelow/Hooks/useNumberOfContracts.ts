bc0d934a48efdfea4828bab72e13611e724c10f8
Faraday <faraday@buffer.finance>
2023-12-06 16:24:16 +0530

    fix

--- src/Views/AboveBelow/Hooks/useNumberOfContracts.ts
+++ src/Views/AboveBelow/Hooks/useNumberOfContracts.ts
@@ -1,73 +1,43 @@
-import { BlackScholes } from '@Utils/Formulas/blackscholes';
 import { toFixed } from '@Utils/NumString';
-import { divide, multiply } from '@Utils/NumString/stringArithmatics';
-import { useCurrentPrice } from '@Views/TradePage/Hooks/useCurrentPrice';
-import { solidityKeccak256 } from 'ethers/lib/utils';
+import { divide } from '@Utils/NumString/stringArithmatics';
 import { useAtomValue } from 'jotai';
-import { getAddress } from 'viem';
 import {
-  selectedExpiry,
   selectedPoolActiveMarketAtom,
   selectedPriceAtom,
   tradeSizeAtom,
 } from '../atoms';
-import { useIV } from './useIV';
-import { useSettlementFee } from './useSettlementFee';
+import { strikePrices } from './useLimitedStrikeArrays';
 
 export const useNumberOfContracts = () => {
   const amount = useAtomValue(tradeSizeAtom);
-  const selectedTimestamp = useAtomValue(selectedExpiry);
   const selectedPrice = useAtomValue(selectedPriceAtom);
   const activeMarket = useAtomValue(selectedPoolActiveMarketAtom);
-  const { data: settlementFees } = useSettlementFee();
-  const { currentPrice } = useCurrentPrice({
-    token0: activeMarket?.token0,
-    token1: activeMarket?.token1,
-  });
-  const { data: ivs } = useIV();
-  if (!amount) return null;
-  if (!selectedTimestamp) return null;
+
   if (!selectedPrice) return null;
   if (!activeMarket) return null;
-  if (!currentPrice) return null;
-  if (!settlementFees) return null;
-  const iv = ivs?.[activeMarket.tv_id];
-  if (iv === undefined) return null;
+
+  const strikes = strikePrices;
+  const activeAssetStrikes = strikes[activeMarket.tv_id];
+  if (!activeAssetStrikes) return null;
 
   const priceObj = selectedPrice[activeMarket.tv_id];
   if (!priceObj) return null;
-  const price = priceObj.price;
-  const currentEpoch = Math.floor(Date.now() / 1000);
-  const expiration = Math.floor(selectedTimestamp / 1000);
-
-  const marketHash = solidityKeccak256(
-    ['uint256', 'uint256'],
-    [toFixed(multiply(price, 8), 0), Math.floor(selectedTimestamp / 1000) + 1]
+  let strikePriceObject = activeAssetStrikes.increasingPriceArray.find(
+    (obj) => obj.strike.toString() == priceObj.price
   );
-  const settlementFee =
-    settlementFees[marketHash + '-' + getAddress(activeMarket.address)];
-  const probability = BlackScholes(
-    true,
-    priceObj.isAbove,
-    currentPrice,
-    price,
-    expiration - currentEpoch,
-    0,
-    iv / 1e4
-  );
-  const sfAbove = settlementFee?.sf_above || settlementFees['Base'] / 1e4;
-  const sfBelow = settlementFee?.sf_below || settlementFees['Base'] / 1e4;
-  const totalFee =
-    probability + (priceObj.isAbove ? sfAbove : sfBelow) * probability;
+  if (!strikePriceObject) {
+    strikePriceObject = activeAssetStrikes.decreasingPriceArray.find(
+      (obj) => obj.strike.toString() == priceObj.price
+    );
+  }
+  if (!strikePriceObject) return null;
+  const totalFee = priceObj.isAbove
+    ? strikePriceObject.totalFeeAbove
+    : strikePriceObject.totalFeeBelow;
+  if (!totalFee) return null;
 
   return {
     contracts: toFixed(divide(amount, totalFee.toString()) as string, 0),
     totalFee,
-    amount,
-    sf: priceObj.isAbove ? sfAbove : sfBelow,
-    currentPrice,
-    strike: price,
-    expiration,
-    isAbove: priceObj.isAbove,
   };
 };
