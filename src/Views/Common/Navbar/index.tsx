3e9859fd11110937a34d94cc54da3b2257618fb2
Faraday <faraday@buffer.finance>
2023-12-09 11:42:17 +0530

    remove onect calls

--- src/Views/Common/Navbar/index.tsx
+++ src/Views/Common/Navbar/index.tsx
@@ -4,7 +4,7 @@ import MemoWalletSVG from '@SVG/Elements/WalletSVG';
 import { inIframe } from '@Utils/isInIframe';
 import { useShutterHandlers } from '@Views/AboveBelow/Components/MobileView/Shutters';
 import { tardesAtom } from '@Views/AboveBelow/Hooks/usePastTradeQuery';
-import { OneCTModal } from '@Views/OneCT/OneCTModal';
+// import { OneCTModal } from '@Views/OneCT/OneCTModal';
 import { useAtomValue } from 'jotai';
 import { useMemo, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -57,7 +57,7 @@ export const Navbar: React.FC<INavbar> = () => {
   const show = !urlSettings?.hide;
   return (
     <header className="  sticky bg-[#232334] top-[0px] flex justify-between w-full h-[45px] pr-[8px] header top-0 z-[102] b1200:z-10">
-      <OneCTModal />
+      {/* <OneCTModal /> */}
       <div className="flex items-center ">
         <div
           role={'button'}
