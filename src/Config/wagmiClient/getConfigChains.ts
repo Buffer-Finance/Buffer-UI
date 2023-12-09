e3ce5d53a55db1392cb766780e31fb3bb2c11bd5
Faraday <faraday@buffer.finance>
2023-12-09 11:30:22 +0530

    replace goerli with sepolia

--- src/Config/wagmiClient/getConfigChains.ts
+++ src/Config/wagmiClient/getConfigChains.ts
@@ -1,20 +1,48 @@
 import { getHashUrlQueryParam } from '@Utils/getHashUrlQueryParam';
 import { isTestnet } from 'config';
-import { arbitrum, arbitrumGoerli, polygon, polygonMumbai } from 'viem/chains';
+import { defineChain } from 'viem';
+import { arbitrum, polygon, polygonMumbai } from 'viem/chains';
 
 export const urlSettings = getHashUrlQueryParam(window.location.href);
+const arbitrumSepolia = defineChain({
+  id: 421614,
+  name: 'Arbitrum Sepolia',
+  network: 'arbitrum-sepolia',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Ether',
+    symbol: 'AETH',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://sepolia-rollup.arbitrum.io/rpc'],
+    },
+    public: {
+      http: ['https://sepolia-rollup.arbitrum.io/rpc'],
+    },
+  },
+  blockExplorers: {
+    default: { name: 'Explorer', url: 'https://api-sepolia.arbiscan.io/api' },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 81930,
+    },
+  },
+});
 
 export function getSupportedChains() {
-  return isTestnet ? [arbitrumGoerli] : [arbitrum];
+  return isTestnet ? [arbitrumSepolia] : [arbitrum];
 }
 
 export const getAllChains = () => {
   switch (urlSettings?.chain) {
     case 'arbitrum':
-      return isTestnet ? [arbitrumGoerli, polygonMumbai] : [arbitrum, polygon];
+      return isTestnet ? [arbitrumSepolia, polygonMumbai] : [arbitrum, polygon];
     case 'polygon':
-      return isTestnet ? [polygonMumbai, arbitrumGoerli] : [polygon, arbitrum];
+      return isTestnet ? [polygonMumbai, arbitrumSepolia] : [polygon, arbitrum];
     default:
-      return isTestnet ? [arbitrumGoerli, polygonMumbai] : [arbitrum, polygon];
+      return isTestnet ? [arbitrumSepolia, polygonMumbai] : [arbitrum, polygon];
   }
 };
