192b1e6a4fec832d94250669f88678f8743bff64
maxwell-1999 <maxwell@buffer.finance>
2023-12-13 17:12:50 +0530

    WIP

--- src/Hooks/AA/getLocalSigner.ts
+++ src/Hooks/AA/getLocalSigner.ts
@@ -1,13 +1,19 @@
 import { ethers } from 'ethers';
 
-export const getLocalSigner = (userAddress: string | `0x${string}`) => {
-  const key = import.meta.env.VITE_STORAGE_KEY;
+export const getLocalSigner = (
+  userAddress: string | `0x${string}`
+): Promise<ethers.Wallet> => {
+  const msg = import.meta.env.VITE_STORAGE_KEY;
   // genearte a privatekey from user address local signer key by hashing them
   console.log(`deb-main- key + userAddress: `, key + userAddress);
   const signer = new ethers.Wallet(
     ethers.utils.keccak256(ethers.utils.toUtf8Bytes(key + userAddress))
   );
   console.log(`deb-main- key + userAddress: `, signer.privateKey);
-
+  // sign the message
+  // const signature = await signer.signMessage(message);
+  // hash the signature and use as public key to create a session signer
+  // const sessionSigner = new ethers.Wallet( ethers.utils.keccak256(signature));
+  // rerurn the session signer
   return signer;
 };
