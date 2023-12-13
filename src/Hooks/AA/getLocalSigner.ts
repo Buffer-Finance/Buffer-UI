217604942937b16bdf4cc8c03731448aab81f7a7
maxwell-1999 <maxwell@buffer.finance>
2023-12-13 17:48:47 +0530

    Siagning step added

--- src/Hooks/AA/getLocalSigner.ts
+++ src/Hooks/AA/getLocalSigner.ts
@@ -1,19 +1,16 @@
+import { SignMessageArgs } from '@wagmi/core';
 import { ethers } from 'ethers';
 
-export const getLocalSigner = (
-  userAddress: string | `0x${string}`
+export const getLocalSigner = async (
+  signMessage: (args?: SignMessageArgs) => Promise<`0x${string}`>
 ): Promise<ethers.Wallet> => {
   const msg = import.meta.env.VITE_STORAGE_KEY;
-  // genearte a privatekey from user address local signer key by hashing them
-  console.log(`deb-main- key + userAddress: `, key + userAddress);
-  const signer = new ethers.Wallet(
-    ethers.utils.keccak256(ethers.utils.toUtf8Bytes(key + userAddress))
-  );
-  console.log(`deb-main- key + userAddress: `, signer.privateKey);
+
   // sign the message
-  // const signature = await signer.signMessage(message);
+  const signature = await signMessage({ message: msg });
+  console.log(`signature: `, signature);
   // hash the signature and use as public key to create a session signer
-  // const sessionSigner = new ethers.Wallet( ethers.utils.keccak256(signature));
+  const sessionSigner = new ethers.Wallet(ethers.utils.keccak256(signature));
   // rerurn the session signer
-  return signer;
+  return sessionSigner;
 };
