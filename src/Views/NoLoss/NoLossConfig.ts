ae73196346693b29e7a09ee21627cb914ab56346
Faraday <faraday@buffer.finance>
2023-12-13 14:31:06 +0530

    update contracts

--- src/Views/NoLoss/NoLossConfig.ts
+++ src/Views/NoLoss/NoLossConfig.ts
@@ -1,14 +1,14 @@
 export const noLossConfig = {
   '421613': {
     graph: {
-      MAIN: 'https://api.thegraph.com/subgraphs/name/buffertech/no-loss-arbitrum-testnet',
+      MAIN: 'https://subgraph.satsuma-prod.com/e66b06ce96d2/bufferfinance/no-loss-sepolia/api',
     },
     multicall: '0xca11bde05977b3631167028862be2a173976ca11',
     router: '0x06eFE941603876Cc3Ff0fa980BACE6DdDE9700B6',
     tournament: {
       manager: '0xfe89eEE509bd01B433B2EC32e6985F1Eb7016Dec',
       leaderboard: '0xe04083caC8be40bbB93FaF9a1d6262C0f09aeCD5',
-      reader: '0x674bE2c9D1C06c4EBEF79F5e76fc47bb60Fd8871',
+      reader: '0x00daF18a3120682F28E4524779087B0955dE3E3d',
     },
     optionsReader: '0x03f5130B77CCA0b91B685913629d97EbBB5f79eD',
   },
