ddf549b9facb94a2ccc23a48f019d2393b8d9ffa
Faraday <faraday@buffer.finance>
2024-01-02 15:51:24 +0530

    fix tradedata line

--- src/Views/AboveBelow/Components/BuyTrade/SelectedTradeData.tsx
+++ src/Views/AboveBelow/Components/BuyTrade/SelectedTradeData.tsx
@@ -4,32 +4,57 @@ import {
   selectedPoolActiveMarketAtom,
   selectedPriceAtom,
 } from '@Views/AboveBelow/atoms';
+import { marketTypeAB } from '@Views/AboveBelow/types';
 import { Display } from '@Views/Common/Tooltips/Display';
 import { useAtomValue } from 'jotai';
 
 export const SelectedTradeData = () => {
-  const selectedTimestamp = useAtomValue(selectedExpiry);
-  const selectedPrice = useAtomValue(selectedPriceAtom);
   const activeMarket = useAtomValue(selectedPoolActiveMarketAtom);
 
-  if (!selectedTimestamp || !selectedPrice || !activeMarket) return <></>;
-  const selectedPriceValue = selectedPrice[activeMarket.tv_id];
-  if (!selectedPriceValue) return <></>;
-  const expiryDateTimeStamp = Math.floor(selectedTimestamp / 1000);
+  if (!activeMarket) return <></>;
   return (
     <span className="text-[#7F87A7] text-f13 mt-3">
-      {activeMarket.token0}/{activeMarket.token1} will be{' '}
-      {selectedPrice[activeMarket.tv_id].isAbove ? 'above' : 'below'}{' '}
+      {activeMarket.token0}/{activeMarket.token1} will be&nbsp;
+      <PriceData activeMarket={activeMarket} />
+      &nbsp;on&nbsp;
+      <DateData />
+    </span>
+  );
+};
+
+const PriceData: React.FC<{ activeMarket: marketTypeAB }> = ({
+  activeMarket,
+}) => {
+  const selectedPrice = useAtomValue(selectedPriceAtom);
+  const priceObj = selectedPrice?.[activeMarket.tv_id];
+  if (priceObj === undefined)
+    return <span className="text-1">( pick a price )</span>;
+  return (
+    <span>
+      {priceObj.isAbove ? 'above' : 'below'}&nbsp;
       <span className="text-1">
         <Display
-          data={selectedPrice[activeMarket.tv_id].price}
+          data={priceObj.price}
           precision={activeMarket.price_precision.toString().length - 1}
           disable
           className="!inline"
         />
-      </span>{' '}
-      on <span className="text-1">{getDisplayDate(expiryDateTimeStamp)}</span>{' '}
-      at <span className="text-1">{getDisplayTime(expiryDateTimeStamp)}</span>
+      </span>
+    </span>
+  );
+};
+
+const DateData = () => {
+  const selectedTimestamp = useAtomValue(selectedExpiry);
+  if (selectedTimestamp === undefined)
+    return <span className="text-1">( pick a date )&nbsp;</span>;
+  const expiryDateTimeStamp = Math.floor(selectedTimestamp / 1000);
+
+  return (
+    <span>
+      <span className="text-1">{getDisplayDate(expiryDateTimeStamp)}</span>{' '}
+      at&nbsp;
+      <span className="text-1">{getDisplayTime(expiryDateTimeStamp)}</span>
     </span>
   );
 };
