0326f896b51ce405ed3b2d1a45fb5e7d15f9906c
Faraday <faraday@buffer.finance>
2024-02-09 11:41:37 +0530

    remove aa account

--- src/Hooks/useUserAccount.ts
+++ src/Hooks/useUserAccount.ts
@@ -3,22 +3,21 @@ import { useAtom } from 'jotai';
 import { useEffect } from 'react';
 import { useAccount } from 'wagmi';
 import { useQuery } from './useQuery';
-import { useSmartAccount } from './AA/useSmartAccount';
 
 export const useUserAccount = () => {
   const { address: account } = useAccount();
   const query = useQuery();
   const [user, setUser] = useAtom(userAtom);
-  const { smartAccount } = useSmartAccount();
+  // const { smartAccount } = useSmartAccount();
   // console.log(`smartWalletAddress: `, smartWalletAddress);
   useEffect(() => {
     const urlAddress = query.get('user_address');
     setUser({
-      userAddress: urlAddress || smartAccount?.address || account,
+      userAddress: urlAddress || account,
       viewOnlyAddress: urlAddress,
       isViewOnlyMode: urlAddress ? true : false,
       connectedWalletAddress: account,
       mainEOA: account,
     });
-  }, [account, query, smartAccount]);
+  }, [account, query]);
 };
