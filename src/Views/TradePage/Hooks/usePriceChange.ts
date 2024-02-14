07cce73bcb6f9bd16ba5fc2d7cfaab6293b8ca9e
Faraday <faraday@buffer.finance>
2024-02-14 18:53:44 +0530

    wip

--- src/Views/TradePage/Hooks/usePriceChange.ts
+++ src/Views/TradePage/Hooks/usePriceChange.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import useSWR from 'swr';
-import { pricePublisherBaseUrl } from '../config';
 import { useMarketsConfig } from './useMarketsConfig';
 
 const usePriceChange = () => {
@@ -9,14 +8,31 @@ const usePriceChange = () => {
     fetcher: async () => {
       if (!markets) return;
 
+      const pythIdsToTvId: { [pythId: string]: string } = {};
+      markets?.forEach((market) => {
+        pythIdsToTvId[market.pythId.slice(2)] = market.tv_id;
+      });
+      // pythIds.push(market.pythId.slice(2))
+      console.log('pythIds', pythIdsToTvId);
+
+      const currentTime = Math.floor(new Date().getTime() / 1000);
+      const { data } = await axios.get(
+        `https://hermes.pyth.network/v2/updates/price/${
+          currentTime - 60
+        }?${Object.keys(pythIdsToTvId)
+          .map((id) => `ids%5B%5D=${id}`)
+          .join('&')}`
+      );
+      console.log('pythIds-data', data);
+
       const queries: string[] = [];
 
       markets?.forEach((market) => queries.push(market.tv_id));
 
-      const { data: response } = await axios.post(
-        pricePublisherBaseUrl + 'bulk_24h_change/',
-        queries
-      );
+      // const { data: response } = await axios.post(
+      //   pricePublisherBaseUrl + 'bulk_24h_change/',
+      //   queries
+      // );
       const finalResponse: {
         [key: string]: {
           pair: string;
@@ -24,12 +40,14 @@ const usePriceChange = () => {
         };
       } = {};
 
-      response.forEach((res: { pair: string; change: number }) => {
-        finalResponse[res.pair] = {
-          pair: res.pair,
-          change: res.change,
-        };
-      });
+      data[0].parsed.forEach((res:))
+
+      // response.forEach((res: { pair: string; change: number }) => {
+      //   finalResponse[res.pair] = {
+      //     pair: res.pair,
+      //     change: res.change,
+      //   };
+      // });
       return finalResponse;
     },
     refreshInterval: 1000 * 60 * 5,
