785a3fd33a9e12602116139c3a632ffc436e2ab6
maxwell-1999 <maxwell@buffer.finance>
2024-03-21 17:14:08 +0530

    WIP

--- src/Views/Jackpot/JackpotChip.tsx
+++ src/Views/Jackpot/JackpotChip.tsx
@@ -1,31 +1,35 @@
 import { Display } from '@Views/Common/Tooltips/Display';
+import { Link } from 'react-router-dom';
 
 const JackpotChip: React.FC<{ jackpote18: string; className?: string }> = ({
   className,
   jackpote18,
 }) => {
+  // const jackpote18 = '10000000000000000000';
   const isJackpotDisabled = jackpote18 ? +jackpote18 == 0 : true;
   if (isJackpotDisabled) return null;
   return (
-    <div
-      className={[
-        'flex items-center bg-[#282b39] px-2 py-[1px] w-fit sm:text-f10 sm:py-[2px]  text-f13 text-[#C3C2D4] font-[500] gap-2 rounded-[6px]',
-        className,
-      ].join(' ')}
-    >
-      <img
+    <Link onClick={(e) => e.stopPropagation()} to={'/Jackpot'}>
+      <div
         className={[
-          'w-[17px] h-[14px] min-w-[17px] min-h-[14px] max-w-[17px] max-h-[14px]',
-          isJackpotDisabled ? 'opacity-30' : '',
+          'flex items-center bg-[#282b39] px-2  py-[1px] w-fit sm:text-f10 sm:py-[2px]  text-f13 text-[#C3C2D4] font-[500] gap-2 rounded-[6px]',
+          className,
         ].join(' ')}
-        src="/JV.png"
-      />
-      <Display
-        data={jackpote18 / 10 ** 18}
-        className="!justify-start"
-        unit={'ARB'}
-      />
-    </div>
+      >
+        <img
+          className={[
+            'w-[18px] h-[15px] min-w-[18px] min-h-[15px] max-w-[18px] max-h-[15px]',
+            isJackpotDisabled ? 'opacity-30' : '',
+          ].join(' ')}
+          src="/JV.png"
+        />
+        <Display
+          data={jackpote18 / 10 ** 18}
+          className="!justify-start hover:underline font-[500]"
+          unit={'ARB'}
+        />
+      </div>
+    </Link>
   );
 };
 
