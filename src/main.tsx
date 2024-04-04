7597b359101e88769a2f5aba39d6d9b4ce63ca74
maxwell-1999 <maxwell@buffer.finance>
2024-04-04 12:10:38 +0530

    WIP

--- src/main.tsx
+++ src/main.tsx
@@ -55,7 +55,8 @@ if (import.meta.env.VITE_MODE === 'production') {
 
     // If the entire session is not sampled, use the below sample rate to sample
     replaysOnErrorSampleRate: 1.0,
-
+    tracesSampleRate: 1.0,
+    denyUrls: [/extensions\//i, /^chrome:\/\//i, /^chrome-extension:\/\//i],
     integrations: [
       new Sentry.Replay({
         // Additional SDK configuration goes in here, for example:
