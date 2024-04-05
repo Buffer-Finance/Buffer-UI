66a449d52d9b6d876c9a3c5ad6cd5ce9d53f727b
maxwell-1999 <maxwell@buffer.finance>
2024-04-05 17:33:48 +0530

    WIP

--- src/main.tsx
+++ src/main.tsx
@@ -44,7 +44,37 @@ import { inject } from '@vercel/analytics';
 import ReloadErrorBoundary from './ReloadErrorBoundry';
 import { BlueBtn } from '@Views/Common/V2-Button';
 inject();
+if (typeof Node === 'function' && Node.prototype) {
+  const originalRemoveChild = Node.prototype.removeChild;
+  Node.prototype.removeChild = function (child) {
+    if (child.parentNode !== this) {
+      if (console) {
+        console.log(
+          'Cannot remove a child from a different parent',
+          child,
+          this
+        );
+      }
+      return child;
+    }
+    return originalRemoveChild.apply(this, arguments);
+  };
 
+  const originalInsertBefore = Node.prototype.insertBefore;
+  Node.prototype.insertBefore = function (newNode, referenceNode) {
+    if (referenceNode && referenceNode.parentNode !== this) {
+      if (console) {
+        console.log(
+          'Cannot insert before a reference node from a different parent',
+          referenceNode,
+          this
+        );
+      }
+      return newNode;
+    }
+    return originalInsertBefore.apply(this, arguments);
+  };
+}
 if (import.meta.env.VITE_MODE === 'production') {
   Sentry.init({
     dsn: import.meta.env.VITE_SENTRY_DSN,
