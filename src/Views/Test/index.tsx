b2a4e567e615970558d3e67fbd01b1bf3002cb9d
maxwell-1999 <maxwell@buffer.finance>
2024-04-05 13:29:19 +0530

    WIP

--- src/Views/Test/index.tsx
+++ src/Views/Test/index.tsx
@@ -1,5 +1,22 @@
+import { Link, LinkProps } from 'react-router-dom';
 import { RenderGraphTestResults } from './GraphTests/RenderGraphTestResults';
-
+interface UTMLinkProps extends LinkProps {
+  src?: string;
+}
+const UTMLink = (props: UTMLinkProps) => {
+  return (
+    <Link
+      {...props}
+      to={`${props.to}?utm_source=${
+        props.src || 'inappredirect'
+      }&utm_medium=redirect&utm_campaign=inapp&utm_term=&utm_content=inapp`}
+    />
+  );
+};
 export const Test = () => {
-  return <RenderGraphTestResults />;
+  return (
+    <UTMLink to="/binary/BTC-USD" src="test-main-btn">
+      Redirect
+    </UTMLink>
+  );
 };
