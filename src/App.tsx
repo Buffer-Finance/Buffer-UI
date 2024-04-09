3485d58f4dc94b30c2b8e893dab154daebaf8fdf
Faraday <faraday@buffer.finance>
2024-04-09 18:28:08 +0530

    add galxe competition notification

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,7 @@ import { ReferralPage } from '@Views/Referral';
 import { defaultMarket } from '@Views/TradePage/config';
 import { i18n } from '@lingui/core';
 import { I18nProvider } from '@lingui/react';
+import { Launch } from '@mui/icons-material';
 import { isTestnet } from 'config';
 import { useMedia } from 'react-use';
 import { useAutoConnect } from './Config/useAutoConnectSafe';
@@ -157,7 +158,7 @@ export const snackAtom = atom<{
 }>({
   message: null,
 });
-const mobileWarningAtom = atomWithLocalStorage('warnign-suer', false);
+const mobileWarningAtom = atomWithLocalStorage('warning-user-galxe', false);
 export const isAutorizedAtom = atomWithStorage('authorized user or not', false);
 
 function App() {
@@ -205,32 +206,24 @@ function App() {
               {snack.message}
             </Alert>
           </Snackbar>
-          {/* {!urlSettings?.hide &&
+          {!urlSettings?.hide &&
             (isMobile && mobileWarningClosed ? false : true) && (
               <Warning
                 body={
-                  <div className="text-center b800:text-start">
-                    🚀 Buffer v2.5 is live on&nbsp;
-                    <a href="https://app.buffer.finance/" target="_blank">
-                      <span className="underline underline-offset-2">
-                        Mainnet
+                  <div className="w-fit flex items-center m-auto">
+                    <span className="bg-[#232334] text-[#10D2FF] text-f11 leading-[16px] px-3 rounded-[6px] font-semibold mr-3">
+                      New
+                    </span>
+                    <a href="/#/leaderboard/galxe" className="m-auto">
+                      <img
+                        src="https://res.cloudinary.com/dtuuhbeqt/image/upload/Leaderboard/bbb.png"
+                        className="mr-3 inline h-[20px] w-[20px] sm:h-[15px] sm:w-[15px]"
+                      />
+                      <span className="text-f14 font-extrabold text-[#232334] leading-[21px] sm:text-[11px] sm:leading-[12px]">
+                        Complete weekly Galxe tasks & trade to win from $14,000
+                        prize pot in the buffer competition.
                       </span>
-                    </a>
-                    &nbsp; | 📜 Learn more about v2.5&nbsp;
-                    <a
-                      href="https://mirror.xyz/0xc730FbdFEb3e9dF76008A19962963cA4A2bd8de2/9v1ATLZoGXbzjLZWQVesWKMwHB4R7yI8XNQfVsyB21o"
-                      target="_blank"
-                    >
-                      <span className="underline underline-offset-2">here</span>
-                    </a>
-                    &nbsp; | ✨ To trade with $BFR as collateral visit the&nbsp;
-                    <a
-                      href="https://classic.app.buffer.finance/"
-                      target="_blank"
-                    >
-                      <span className="underline underline-offset-2">
-                        classic version
-                      </span>
+                      <Launch className="scale-[0.85] text-[#232334] ml-1 mb-1" />
                     </a>
                   </div>
                 }
@@ -239,9 +232,9 @@ function App() {
                 }}
                 shouldAllowClose={true}
                 state={!mobileWarningClosed}
-                className="disclaimer"
+                className="disclaimer !bg-[#10D2FF] !text-[#232334]"
               />
-            )} */}
+            )}
           <TnCModal />
           <SideBar />
         </Background>
