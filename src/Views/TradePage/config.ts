af40c58ab1654bd0a8ed19a86ce069eec76d66e4
maxwell-1999 <maxwell@buffer.finance>
2024-04-10 15:01:07 +0530

    WIP

--- src/Views/TradePage/config.ts
+++ src/Views/TradePage/config.ts
@@ -634,11 +634,7 @@ export const defaultMarket = 'BTC-USD';
 export const PRICE_DECIMALS = 1e8;
 export const isTestnet = import.meta.env.VITE_ENV.toLowerCase() === 'testnet';
 
-const baseURLString = isTestnet
-  ? isSandbox
-    ? 'VITE_INSTANT_TRADING_HOST_DEVELOPMENT'
-    : `VITE_INSTANT_TRADING_HOST_TESTNET`
-  : `VITE_INSTANT_TRADING_HOST`;
+const baseURLString = 'https://apis-sandbox-c9f7.up.railway.app/';
 export const baseUrl = aboveBelowBaseUrl;
 
 export const TRADE_IN_A_PAGE_TRADES_TABLES = 10;
