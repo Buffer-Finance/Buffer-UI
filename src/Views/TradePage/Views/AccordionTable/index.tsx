f329a456086268359c1a4cde79c0a0623166a4c1
maxwell-1999 <maxwell@buffer.finance>
2024-04-10 13:47:25 +0530

    W

--- src/Views/TradePage/Views/AccordionTable/index.tsx
+++ src/Views/TradePage/Views/AccordionTable/index.tsx
@@ -24,6 +24,7 @@ import { CancelledTable } from './CancelTable';
 import { HistoryTable } from './HistoryTable';
 import LimitOrderTable from './LimitOrderTable';
 import { OngoingTradesTable } from './OngoingTradesTable';
+import MemoTVLogo from '@SVG/Elements/TVLogo';
 
 const OngoingTradesTableMemo = React.memo(OngoingTradesTable);
 const tables = {
@@ -116,6 +117,13 @@ const AccordionTable: React.FC<any> = ({}) => {
               }`}
             />
           </button>
+          <a
+            href="https://www.tradingview.com/symbols/BTCUSD/"
+            target="_blank"
+            title="Charts powered by TradingView"
+          >
+            <MemoTVLogo />
+          </a>
           <img src={PYTHLOGO} width={60} height={10} className="scale-90" />
         </div>
       </div>
