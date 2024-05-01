ca5fef66a53b76fac909a3e0e184f1ed55dd5333
maxwell-1999 <maxwell@buffer.finance>
2024-05-01 18:51:23 +0530

    WIP

--- src/Views/Rewards/config.ts
+++ src/Views/Rewards/config.ts
@@ -1,3 +1,4 @@
 export const startWeekId = 2824;
-export const rebatesAddress = '0x684E76C4Edf04Dc73CA2233aCC18B7cA4fB66DB9';
-export const competitionRewardAddress = '0x1E8B0352d951fe53685A0181e333E9645B9ce24C';
+export const rebatesAddress = '0xA606A6172Db72b86ce2228d95Ab58Fe5ff13A4BB';
+export const competitionRewardAddress =
+  '0x1E8B0352d951fe53685A0181e333E9645B9ce24C';
