9f855d4c832a601aa25aad9bde44b28f0d60b03f
Faraday <faraday@buffer.finance>
2024-05-03 15:15:49 +0530

    update rewards weekId

--- src/Views/Rewards/config.ts
+++ src/Views/Rewards/config.ts
@@ -1,4 +1,4 @@
-export const startWeekId = 2824;
+export const startWeekId = 2834;
 export const rebatesAddress = '0x860c5D96408D8C8FB560Ba66ae54E57d049A3471';
 export const competitionRewardAddress =
   '0x1E8B0352d951fe53685A0181e333E9645B9ce24C';
