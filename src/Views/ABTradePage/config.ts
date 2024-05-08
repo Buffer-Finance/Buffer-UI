447c024429503376701f666fb8ecf5b51d200a50
maxwell-1999 <maxwell@buffer.finance>
2024-05-08 13:37:56 +0530

    WIP

--- src/Views/ABTradePage/config.ts
+++ src/Views/ABTradePage/config.ts
@@ -7,7 +7,7 @@ export const isSandbox =
 export const appConfig = {
   '421614': {
     graph: {
-      MAIN: udgraph,
+      MAIN: 'https://subgraph.satsuma-prod.com/e66b06ce96d2/bufferfinance/v2.6-arbitrum-one/version/v0.0.4-ud-trades-markets/api',
       ABOVE_BELOW: ABGraph,
       EVENTS:
         'https://subgraph.satsuma-prod.com/e66b06ce96d2/bufferfinance/up-down-v3-testnet/version/platform-evts-ab/api',
