7ebb371726b3ab2fe5cd0e869366e9e3e00f953e
Faraday <faraday@buffer.finance>
2024-05-08 14:53:27 +0530

    update leaderboard graph url

--- src/Views/TradePage/config.ts
+++ src/Views/TradePage/config.ts
@@ -122,7 +122,7 @@ export const appConfig = {
       EVENTS:
         'https://subgraph.satsuma-prod.com/bufferfinance/events/version/v2.5-up-events-mainnet/api',
       REWARDS: `https://subgraph.satsuma-prod.com/e66b06ce96d2/bufferfinance/jackpot/api`,
-      LEADERBOARD: `https://subgraph.satsuma-prod.com/e66b06ce96d2/bufferfinance/jackpot/version/v3.0.31-leaderboard-tracking-update-contracts/api`,
+      LEADERBOARD: `https://subgraph.satsuma-prod.com/e66b06ce96d2/bufferfinance/v2.6-arbitrum-one/version/v0.0.5-leaderboard-tracking/api`,
     },
     product_id: {
       UP_DOWN: {
