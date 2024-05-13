165698e2799506da6d65b9f4ce660dd8940314c6
maxwell-1999 <maxwell@buffer.finance>
2024-05-13 12:13:09 +0530

    WIP

--- src/Config/getTabs.tsx
+++ src/Config/getTabs.tsx
@@ -320,7 +320,7 @@ export const getTabs = (
         isExternalLink: false,
       },
       {
-        to: 'https://ab.buffer.finance',
+        to: '/ab/' + market,
         name: 'Above/Below',
         subTabs: [],
         isExternalLink: true,
