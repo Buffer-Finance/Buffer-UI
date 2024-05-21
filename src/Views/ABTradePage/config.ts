7ca1795afc868cbaa9f873e09ccdf3206331891a
maxwell-1999 <maxwell@buffer.finance>
2024-05-21 17:43:09 +0530

    WIP

--- src/Views/ABTradePage/config.ts
+++ src/Views/ABTradePage/config.ts
@@ -1,59 +1,23 @@
 import { ABGraph } from 'src/Config/Graphurls';
 import { notificationPosition, tradePanelPosition } from './type';
 import { udgraph } from '@Views/TradePage/config';
+import { ContractsConfig } from '@ConfigContract';
 
 export const isSandbox =
   import.meta.env.VITE_DEV_ENV.toLowerCase() === 'sandbox';
 export const appConfig = {
   '421614': {
-    graph: {
-      MAIN: 'https://subgraph.satsuma-prod.com/e66b06ce96d2/bufferfinance/above-below-sepolia/api',
-      ABOVE_BELOW: ABGraph,
-      EVENTS:
-        'https://subgraph.satsuma-prod.com/e66b06ce96d2/bufferfinance/up-down-v3-testnet/version/platform-evts-ab/api',
-    },
+    graph: ContractsConfig['421614'].graph,
     multicall: '0xca11bde05977b3631167028862be2a173976ca11',
     referral_storage: '0x50880Adb1e67cDebc1fcd57727AB99740e458322',
-    router: '0xc8083f764d5ce46E4DbEa3dEB975C3301c8d08aB',
-    signer_manager: '0x4499D65378E7D6ECb152caB3893dfFBB654F9961',
+    router: ContractsConfig['421614'].ab.router,
+    signer_manager: ContractsConfig['421614'].signer_manager,
     booster: '0xda0E515d5d4217b2fF634aa21A8aeA8a74cd2808',
     config_setter: '0xf5FE716462112a3352926F63d92b51293ac5d006',
-    v2_router: '0xc8083f764d5ce46E4DbEa3dEB975C3301c8d08aB',
-    above_below_router: '0xc8083f764d5ce46E4DbEa3dEB975C3301c8d08aB',
-    poolsInfo: {
-      '0x464c93cab18A051a24BD520bb97c22C583b48F01': {
-        tokenAddress: '0xb180dB4293D9247Dc974F1445082ae55A91C9539',
-        faucet: '0xAb4df8Aaa1F54E84C469f4bc0e513436088C9B86',
-        decimals: 6,
-        token: 'USDC.e',
-        permitName: 'USD Coin (Arb1)',
-        is_pol: false,
-      },
-      '0xEe8f62C080A6da2B91ab3478D58e6999daAAb8be': {
-        tokenAddress: '0x7Fe2dD3b4619802B8c4b404f5e440756391439ba',
-        faucet: '0xB54521E255d23F2fA7f37d29C7E76D8FFa85fc05',
-        decimals: 6,
-        token: 'USDC',
-        permitName: 'USDC',
-        is_pol: false,
-      },
-      '0x70086DFD2b089359A6582A18D24aBE1AcE40f8D0': {
-        tokenAddress: '0x9132016099CEbb740db64A36da0D3307824Ad159',
-        faucet: '0x62Db9CD484b3B59e1d0444cea1f0D0D3c00bf2F5',
-        decimals: 18,
-        token: 'BFR',
-        permitName: 'Token',
-        is_pol: false,
-      },
-      '0x52126176479d8aFADF2Bc32eDe79dfDdFe69189c': {
-        tokenAddress: '0xd8F5E01e1723EFDdc7faA76a8C3FeFb4A8ef5b76',
-        faucet: '0x6B655D99962F58B9Aa0fFB18281408CdBCf61800',
-        decimals: 18,
-        token: 'ARB',
-        permitName: 'ARB',
-        is_pol: false,
-      },
-    },
+    v2_router: ContractsConfig['421614'].ab.router,
+    above_below_router: ContractsConfig['421614'].ab.router,
+    poolsInfo: ContractsConfig['421614'].poolsInfo,
+
     EarnConfig: {
       RewardRouter: '0x2033f87A0956388C25Ce5A21F97EE78DD37c5D7f',
       BLP: '0xb2685B520Eb93769755b0B2c96dca1D10459F378',
@@ -91,21 +55,16 @@ export const appConfig = {
     },
   },
   '42161': {
-    graph: {
-      MAIN: 'https://subgraph.satsuma-prod.com/e66b06ce96d2/bufferfinance/v2.6-arbitrum-one/version/v0.0.4-ud-trades-markets/api',
-      ABOVE_BELOW: ABGraph,
-      EVENTS:
-        'https://subgraph.satsuma-prod.com/e66b06ce96d2/bufferfinance/events/api',
-    },
+    graph: ContractsConfig['42161'].graph,
     multicall: '0x842eC2c7D803033Edf55E478F461FC547Bc54EB2',
     referral_storage: '0xFea57B9548cd72D8705e4BB0fa83AA35966D9c29',
-    router: '0x94582981c3be6092b912265C2d2cE172e7f9c3B1',
-    signer_manager: '0x84cb6d8Fafa09D8A606f423feD6BB2745e677526',
+    router: ContractsConfig['42161'].ab.router,
+    signer_manager: ContractsConfig['42161'].signer_manager,
     booster: '0x1FE0A88372A75926dc26dFF369B0b3aC5569F669',
     config_setter: '0xf5FE716462112a3352926F63d92b51293ac5d006',
-    v2_router: '0x94582981c3be6092b912265C2d2cE172e7f9c3B1',
-    jackpot: '0xEa0b956A04b265A7E2A6d00A4bD65a48C430cE16',
-    above_below_router: '0x94582981c3be6092b912265C2d2cE172e7f9c3B1',
+    v2_router: ContractsConfig['42161'].ab.router,
+    jackpot: ContractsConfig['42161'].jackpot,
+    above_below_router: ContractsConfig['42161'].ab.router,
     product_id: {
       UP_DOWN: {
         id: 2,
@@ -124,40 +83,8 @@ export const appConfig = {
         router: '0xd6705927d296605bE4b590a6841DF6dD967a6959',
       },
     },
-    poolsInfo: {
-      '0x6Ec7B10bF7331794adAaf235cb47a2A292cD9c7e': {
-        tokenAddress: '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8',
-        faucet: '0x0000000000000000000000000000000000000000',
-        decimals: 6,
-        token: 'USDC.e',
-        permitName: 'USD Coin (Arb1)',
-        is_pol: false,
-      },
-      '0xEe8f62C080A6da2B91ab3478D58e6999daAAb8be': {
-        tokenAddress: '0x7Fe2dD3b4619802B8c4b404f5e440756391439ba',
-        faucet: '0xB54521E255d23F2fA7f37d29C7E76D8FFa85fc05',
-        decimals: 6,
-        token: 'USDC',
-        permitName: 'USDC',
-        is_pol: false,
-      },
-      '0x70086DFD2b089359A6582A18D24aBE1AcE40f8D0': {
-        tokenAddress: '0x9132016099CEbb740db64A36da0D3307824Ad159',
-        faucet: '0x62Db9CD484b3B59e1d0444cea1f0D0D3c00bf2F5',
-        decimals: 18,
-        token: 'BFR',
-        permitName: 'Token',
-        is_pol: false,
-      },
-      '0xaE0628C88EC6C418B3F5C005f804E905f8123833': {
-        tokenAddress: '0x912CE59144191C1204E64559FE8253a0e49E6548',
-        faucet: '0x6B655D99962F58B9Aa0fFB18281408CdBCf61800',
-        decimals: 18,
-        token: 'ARB',
-        permitName: 'Arbitrum',
-        is_pol: false,
-      },
-    },
+    poolsInfo: ContractsConfig['42161'].poolsInfo,
+
     EarnConfig: {
       RewardRouter: '0xbD5FBB3b2610d34434E316e1BABb9c3751567B67',
       BLP: '0x6Ec7B10bF7331794adAaf235cb47a2A292cD9c7e',
--- src/Views/TradePage/config.ts
+++ src/Views/TradePage/config.ts
@@ -1,6 +1,7 @@
 import { ABGraph } from 'src/Config/Graphurls';
 import { notificationPosition, tradePanelPosition } from './type';
 import { ABBaseURL as ABBaseURLCopy } from '@Views/ABTradePage/config';
+import { ContractsConfig } from '@ConfigContract';
 export const aboveBelowBaseUrl = ABBaseURLCopy;
 export const isSandbox =
   import.meta.env.VITE_DEV_ENV.toLowerCase() === 'sandbox';
@@ -8,27 +9,16 @@ export const udgraph =
   'https://subgraph.satsuma-prod.com/e66b06ce96d2/bufferfinance/v2.6-arbitrum-one/version/v0.0.4-ud-trades-markets/api';
 export const appConfig = {
   '421614': {
-    graph: {
-      ABOVE_BELOW: ABGraph,
-
-      MAIN: `https://subgraph.satsuma-prod.com/e66b06ce96d2/bufferfinance/above-below-sepolia/version/v0.2.1-ud-updat3e-contracts/api`,
-      EVENTS:
-        'https://subgraph.satsuma-prod.com/e66b06ce96d2/bufferfinance/up-down-v3-testnet/version/platform-evts/api',
-      REWARDS: `https://subgraph.satsuma-prod.com/e66b06ce96d2/bufferfinance/jackpot/api`,
-      LEADERBOARD: `https://subgraph.satsuma-prod.com/e66b06ce96d2/bufferfinance/jackpot/version/v3.0.31-leaderboard-tracking-update-contracts/api`,
-      DASHBOARD: '',
-      PROFILE: '',
-      LP: `https://subgraph.satsuma-prod.com/e66b06ce96d2/bufferfinance/jackpot/version/v3.0.32-pool-tracking-update-blp-rate/api`,
-    },
+    graph: ContractsConfig['421614'].graph,
     multicall: '0xca11bde05977b3631167028862be2a173976ca11',
     referral_storage: '0x50880Adb1e67cDebc1fcd57727AB99740e458322',
-    router: '0x693351786cf6f328B1495bd9d527d898929580AE',
-    signer_manager: '0x4499D65378E7D6ECb152caB3893dfFBB654F9961',
+    router: ContractsConfig['421614'].ud.router,
+    signer_manager: ContractsConfig['421614'].signer_manager,
     booster: '0xbE96E1260090Ec873ed9F246f548fcF67a7da148',
     config_setter: '0xf5FE716462112a3352926F63d92b51293ac5d006',
-    v2_router: '0x693351786cf6f328B1495bd9d527d898929580AE',
-    jackpot: '0x6D09f5606e2190e0E42E0ebD89250748981eec3C',
-    above_below_router: '0xd6705927d296605bE4b590a6841DF6dD967a6959',
+    v2_router: ContractsConfig['421614'].ud.router,
+    jackpot: ContractsConfig['421614'].jackpot,
+    above_below_router: ContractsConfig['421614'].ab.router,
     product_id: {
       UP_DOWN: {
         id: 2,
@@ -36,7 +26,7 @@ export const appConfig = {
         metaData: {},
         name: 'UP_DOWN',
         product_id: 'abc',
-        router: '0x693351786cf6f328B1495bd9d527d898929580AE',
+        router: ContractsConfig['421614'].ud.router,
       },
       AB: {
         id: 1,
@@ -44,43 +34,10 @@ export const appConfig = {
         metaData: {},
         name: 'AB',
         product_id: 'xyz',
-        router: '0xd6705927d296605bE4b590a6841DF6dD967a6959',
+        router: ContractsConfig['421614'].ab.router,
       },
     },
-    poolsInfo: {
-      '0x464c93cab18A051a24BD520bb97c22C583b48F01': {
-        tokenAddress: '0xb180dB4293D9247Dc974F1445082ae55A91C9539',
-        faucet: '0xAb4df8Aaa1F54E84C469f4bc0e513436088C9B86',
-        decimals: 6,
-        token: 'USDC.e',
-        permitName: 'USDC',
-        is_pol: false,
-      },
-      '0xEe8f62C080A6da2B91ab3478D58e6999daAAb8be': {
-        tokenAddress: '0x7Fe2dD3b4619802B8c4b404f5e440756391439ba',
-        faucet: '0xB54521E255d23F2fA7f37d29C7E76D8FFa85fc05',
-        decimals: 6,
-        token: 'USDC',
-        permitName: 'USDC',
-        is_pol: false,
-      },
-      '0x70086DFD2b089359A6582A18D24aBE1AcE40f8D0': {
-        tokenAddress: '0x9132016099CEbb740db64A36da0D3307824Ad159',
-        faucet: '0x62Db9CD484b3B59e1d0444cea1f0D0D3c00bf2F5',
-        decimals: 18,
-        token: 'BFR',
-        permitName: 'Token',
-        is_pol: false,
-      },
-      '0x52126176479d8aFADF2Bc32eDe79dfDdFe69189c': {
-        tokenAddress: '0xd8F5E01e1723EFDdc7faA76a8C3FeFb4A8ef5b76',
-        faucet: '0x6B655D99962F58B9Aa0fFB18281408CdBCf61800',
-        decimals: 18,
-        token: 'ARB',
-        permitName: 'ARB',
-        is_pol: false,
-      },
-    },
+    poolsInfo: ContractsConfig['421614'].poolsInfo,
     EarnConfig: {
       RewardRouter: '0x2033f87A0956388C25Ce5A21F97EE78DD37c5D7f',
       BLP: '0xb2685B520Eb93769755b0B2c96dca1D10459F378',
@@ -118,19 +75,7 @@ export const appConfig = {
     },
   },
   '42161': {
-    graph: {
-      ABOVE_BELOW: ABGraph,
-      MAIN: udgraph,
-      EVENTS:
-        'https://subgraph.satsuma-prod.com/e66b06ce96d2/bufferfinance/events/version/v2.5-up-events-mainnet/api',
-      REWARDS: `https://subgraph.satsuma-prod.com/e66b06ce96d2/bufferfinance/jackpot/api`,
-      LEADERBOARD: `https://subgraph.satsuma-prod.com/e66b06ce96d2/bufferfinance/v2.6-arbitrum-one/version/v0.0.5-leaderboard-tracking/api`,
-      DASHBOARD:
-        'https://subgraph.satsuma-prod.com/e66b06ce96d2/bufferfinance/v2.5-arbitrum-mainnet/version/v2.9.1-ud-ab-nfts-leagues-stats-defillama-merge/api',
-      PROFILE:
-        'https://subgraph.satsuma-prod.com/e66b06ce96d2/bufferfinance/arbitrum-mainnet/version/v2.7.2-v2.6-profile-data/api',
-      LP: '',
-    },
+    graph: ContractsConfig[42161].graph,
     product_id: {
       UP_DOWN: {
         id: 2,
@@ -138,7 +83,7 @@ export const appConfig = {
         metaData: {},
         name: 'UP_DOWN',
         product_id: 'abc',
-        router: '0x2BAA48961C1CD376484b601278bF7A51E94293a9',
+        router: ContractsConfig[42161].ud.router,
       },
       AB: {
         id: 1,
@@ -146,60 +91,19 @@ export const appConfig = {
         metaData: {},
         name: 'AB',
         product_id: 'xyz',
-        router: '0x94582981c3be6092b912265C2d2cE172e7f9c3B1',
+        router: ContractsConfig[42161].ab.router,
       },
     },
     multicall: '0xca11bde05977b3631167028862be2a173976ca11',
     referral_storage: '0xFea57B9548cd72D8705e4BB0fa83AA35966D9c29',
-    router: '0x2BAA48961C1CD376484b601278bF7A51E94293a9',
-    signer_manager: '0x84cb6d8Fafa09D8A606f423feD6BB2745e677526',
+    router: ContractsConfig[42161].ud.router,
+    signer_manager: ContractsConfig[42161].signer_manager,
     booster: '0x1FE0A88372A75926dc26dFF369B0b3aC5569F669',
     config_setter: '0xf5FE716462112a3352926F63d92b51293ac5d006',
-    v2_router: '0x2BAA48961C1CD376484b601278bF7A51E94293a9',
+    v2_router: ContractsConfig[42161].ud.router,
     jackpot: '0xEa0b956A04b265A7E2A6d00A4bD65a48C430cE16',
-    above_below_router: '0x94582981c3be6092b912265C2d2cE172e7f9c3B1',
-    poolsInfo: {
-      '0x6Ec7B10bF7331794adAaf235cb47a2A292cD9c7e': {
-        tokenAddress: '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8',
-        faucet: '0x0000000000000000000000000000000000000000',
-        decimals: 6,
-        token: 'USDC.e',
-        permitName: 'USD Coin (Arb1)',
-        is_pol: false,
-      },
-      '0xfD9f8841C471Fcc55f5c09B8ad868BdC9eDeBDE1': {
-        tokenAddress: '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8',
-        faucet: null,
-        decimals: 6,
-        token: 'USDC',
-        is_pol: true,
-      },
-      '0xEe8f62C080A6da2B91ab3478D58e6999daAAb8be': {
-        tokenAddress: '0x7Fe2dD3b4619802B8c4b404f5e440756391439ba',
-        faucet: '0xB54521E255d23F2fA7f37d29C7E76D8FFa85fc05',
-        decimals: 6,
-        token: 'USDC',
-        permitName: 'USDC',
-        is_pol: false,
-      },
-      '0x70086DFD2b089359A6582A18D24aBE1AcE40f8D0': {
-        tokenAddress: '0x9132016099CEbb740db64A36da0D3307824Ad159',
-        faucet: '0x62Db9CD484b3B59e1d0444cea1f0D0D3c00bf2F5',
-        decimals: 18,
-        token: 'BFR',
-        permitName: 'Token',
-        is_pol: false,
-      },
-      '0xaE0628C88EC6C418B3F5C005f804E905f8123833': {
-        tokenAddress: '0x912CE59144191C1204E64559FE8253a0e49E6548',
-        faucet: '0x6B655D99962F58B9Aa0fFB18281408CdBCf61800',
-        decimals: 18,
-        token: 'ARB',
-        permitName: 'Arbitrum',
-        is_pol: false,
-      },
-    },
-
+    above_below_router: ContractsConfig[42161].ab.router,
+    poolsInfo: ContractsConfig['42161'].poolsInfo,
     EarnConfig: {
       RewardRouter: '0xbD5FBB3b2610d34434E316e1BABb9c3751567B67',
       BLP: '0x6Ec7B10bF7331794adAaf235cb47a2A292cD9c7e',
