ac20c6eb5f3586a77e150fd2be80a6c848319c5a
gakshita <curie@buffer.finance>
2024-05-27 18:32:34 +0530

    Fix: Price precision for FOrex

--- src/Views/TradePage/config.ts
+++ src/Views/TradePage/config.ts
@@ -298,6 +298,7 @@ export const marketsForChart = {
     pythId:
       '0xe62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43',
     pythGroup: 'Crypto',
+    expo: 8,
   },
   ETHUSD: {
     category: 'Crypto',
@@ -311,6 +312,7 @@ export const marketsForChart = {
     pythId:
       '0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace',
     pythGroup: 'Crypto',
+    expo: 8,
   },
   EURUSD: {
     category: 'Forex',
@@ -324,6 +326,7 @@ export const marketsForChart = {
     pythId:
       '0xa995d00bb36a63cef7fd2c287dc105fc8f3d93779f062f09551b0af3e81ec30b',
     pythGroup: 'FX',
+    expo: 5,
   },
   GBPUSD: {
     category: 'Forex',
@@ -337,6 +340,7 @@ export const marketsForChart = {
     pythId:
       '0x84c2dde9633d93d1bcad84e7dc41c9d56578b7ec52fabedc1f335d673df0a7c1',
     pythGroup: 'FX',
+    expo: 5,
   },
   SOLUSD: {
     category: 'Crypto',
@@ -350,6 +354,7 @@ export const marketsForChart = {
     pythId:
       '0xef0d8b6fda2ceba41da15d4095d1da392a0d2f8ed0c6c7bc0f4cfac8c280b56d',
     pythGroup: 'Crypto',
+    expo: 8,
   },
   LINKUSD: {
     category: 'Crypto',
@@ -363,6 +368,7 @@ export const marketsForChart = {
     pythId:
       '0x8ac0c70fff57e9aefdf5edf44b51d62c2d433653cbb2cf5cc06bb115af04d221',
     pythGroup: 'Crypto',
+    expo: 8,
   },
   XAUUSD: {
     category: 'Commodity',
@@ -376,6 +382,7 @@ export const marketsForChart = {
     pythId:
       '0x765d2ba906dbc32ca17cc11f5310a89e9ee1f6420508c63861f2f8ba4ee34bb2',
     pythGroup: 'Metal',
+    expo: 3,
   },
   XAGUSD: {
     category: 'Commodity',
@@ -389,6 +396,7 @@ export const marketsForChart = {
     pythId:
       '0xf2fb02c32b055c805e7238d628e5e9dadef274376114eb1f012337cabe93871e',
     pythGroup: 'Metal',
+    expo: 5,
   },
   AUDUSD: {
     category: 'Forex',
@@ -402,6 +410,7 @@ export const marketsForChart = {
     pythId:
       '0x67a6f93030420c1c9e3fe37c1ab6b77966af82f995944a9fefce357a22854a80',
     pythGroup: 'FX',
+    expo: 5,
   },
   USDJPY: {
     category: 'Forex',
@@ -415,6 +424,7 @@ export const marketsForChart = {
     pythId:
       '0xef2c98c804ba503c6a707e38be4dfbb16683775f195b091252bf24693042fd52',
     pythGroup: 'FX',
+    expo: 3,
   },
   NZDUSD: {
     category: 'Forex',
@@ -428,6 +438,7 @@ export const marketsForChart = {
     pythId:
       '0x92eea8ba1b00078cdc2ef6f64f091f262e8c7d0576ee4677572f314ebfafa4c7',
     pythGroup: 'FX',
+    expo: 5,
   },
   USDCHF: {
     category: 'Forex',
@@ -441,6 +452,7 @@ export const marketsForChart = {
     pythId:
       '0x0b1e3297e69f162877b577b0d6a47a0d63b2392bc8499e6540da4187a63e28f8',
     pythGroup: 'FX',
+    expo: 5,
   },
   USDCAD: {
     category: 'Forex',
@@ -454,6 +466,7 @@ export const marketsForChart = {
     pythId:
       '0x3112b03a41c910ed446852aacf67118cb1bec67b2cd0b9a214c58cc0eaa2ecca',
     pythGroup: 'FX',
+    expo: 5,
   },
   ARBUSD: {
     category: 'Crypto',
@@ -467,6 +480,7 @@ export const marketsForChart = {
     pythId:
       '0x3fa4252848f9f0a1480be62745a4629d9eb1322aebab8a791e344b3b9c1adcf5',
     pythGroup: 'Crypto',
+    expo: 8,
   },
   BNBUSD: {
     category: 'Crypto',
@@ -480,6 +494,7 @@ export const marketsForChart = {
     pythId:
       '0x2f95862b045670cd22bee3114c39763a4a08beeb663b145d283c31d7d1101c4f',
     pythGroup: 'Crypto',
+    expo: 8,
   },
   MATICUSD: {
     category: 'Crypto',
@@ -493,6 +508,7 @@ export const marketsForChart = {
     pythId:
       '0x5de33a9112c2b700b8d30b8a3402c103578ccfa2765696471cc672bd5cf6ac52',
     pythGroup: 'Crypto',
+    expo: 8,
   },
   OPUSD: {
     category: 'Crypto',
@@ -506,6 +522,7 @@ export const marketsForChart = {
     pythId:
       '0x385f64d993f7b77d8182ed5003d97c60aa3361f3cecfe711544d2d59165e9bdf',
     pythGroup: 'Crypto',
+    expo: 8,
   },
   XRPUSD: {
     category: 'Crypto',
@@ -519,6 +536,7 @@ export const marketsForChart = {
     pythId:
       '0xec5d399846a9209f3fe5881d70aae9268c94339ff9817e8d18ff19fa05eea1c8',
     pythGroup: 'Crypto',
+    expo: 8,
   },
   DOGEUSD: {
     category: 'Crypto',
@@ -532,6 +550,7 @@ export const marketsForChart = {
     pythId:
       '0xdcef50dd0a4cd2dcc17e45df1676dcb336a11a61c69df7a0299b0150c672d25c',
     pythGroup: 'Crypto',
+    expo: 8,
   },
   TONUSD: {
     category: 'Crypto',
@@ -545,6 +564,7 @@ export const marketsForChart = {
     pythId:
       '0x8963217838ab4cf5cadc172203c1f0b763fbaa45f346d8ee50ba994bbcac3026',
     pythGroup: 'Crypto',
+    expo: 8,
   },
   SHIBUSD: {
     category: 'Crypto',
@@ -558,6 +578,7 @@ export const marketsForChart = {
     pythId:
       '0xf0d57deca57b3da2fe63a493f4c25925fdfd8edf834b20f93e1f84dbd1504d4a',
     pythGroup: 'Crypto',
+    expo: 10,
   },
   USDSGD: {
     category: 'Forex',
@@ -571,6 +592,7 @@ export const marketsForChart = {
     pythGroup: 'FX',
     pythId:
       '0x396a969a9c1480fa15ed50bc59149e2c0075a72fe8f458ed941ddec48bdb4918',
+    expo: 5,
   },
 } as const;
 
