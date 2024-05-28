f096954d853aa37fc175025f916bbbdda2d52c63
gakshita <curie@buffer.finance>
2024-05-28 15:41:04 +0530

    Feat: Updated lp contracts

--- src/Views/LpRewards/config.ts
+++ src/Views/LpRewards/config.ts
@@ -7,9 +7,9 @@ export const lpConfig = {
   '421614': {
     USDC: '0x7Fe2dD3b4619802B8c4b404f5e440756391439ba',
     uBLP: '0xEe8f62C080A6da2B91ab3478D58e6999daAAb8be',
-    RewardRouter: '0x6833f1d342efDFeEb44eD9830c1b4F5A06292e51',
-    nftLockPool: '0x48354099b3cF94c7dF28225aebb79257f4E81b04',
-    feeBLPtracker: '0xaF720639191E5f4Ab220e61Af2Cf07995D52d805',
+    RewardRouter: '0xa4BED1D57b4868B25716af264aC82d9d5262c928',
+    nftLockPool: '0xd4bf543d10654e0Ef47AE606b963C214f6552E1A',
+    feeBLPtracker: '0x4cdb37E0B33eb763918c6225CE81fcF788cd097E',
   },
 };
 
