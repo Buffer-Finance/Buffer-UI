2ae83848a3cb25245392a7efb98b4271e05037e4
gakshita <curie@buffer.finance>
2024-05-30 12:20:31 +0530

    Feat: Mainnet contracts for LP

--- src/Views/LpRewards/config.ts
+++ src/Views/LpRewards/config.ts
@@ -11,6 +11,13 @@ export const lpConfig = {
     nftLockPool: '0xd4bf543d10654e0Ef47AE606b963C214f6552E1A',
     feeBLPtracker: '0x4cdb37E0B33eb763918c6225CE81fcF788cd097E',
   },
+  '42161': {
+    USDC: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
+    uBLP: '0x9501a00d7d4BC7558196B2e4d61c0ec5D16dEfb2',
+    RewardRouter: '0xfd01f94a0fa92374ed4a9c28000Db5a7814FF11b',
+    nftLockPool: '0xC428617D64b6609eF4265a2104eAa09202dB5160',
+    feeBLPtracker: '0xb4e4fA0Ba9333b6a72cCd2F758a0600606B0E0E3',
+  },
 };
 
 export const getLpConfig = (chainId: string | number) => {
