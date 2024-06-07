099e064923e2d7d0fbb590c8b11e38baa774576a
maxwell-1999 <maxwell@buffer.finance>
2024-06-07 14:05:21 +0530

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,20 @@ import { Earn } from '@Views/Earn';
 import SideBar from '@Views/Common/Sidebar';
 import { ShareIcon } from '@Views/Common/Navbar/AccountDropdown';
 import { Test } from './test';
+import React from 'react';
+
+function ExternalLinkSVG(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg width={11} height={11} fill="none" {...props}>
+      <path
+        d="M7.07 0a.788.788 0 00-.557 1.343l1.016 1.014-3.37 3.374A.787.787 0 005.27 6.843L8.64 3.47l1.017 1.017A.786.786 0 0011 3.929V.786A.785.785 0 0010.214 0H7.07zM1.964.786A1.964 1.964 0 000 2.75v6.286C0 10.12.879 11 1.964 11h6.284a1.964 1.964 0 001.964-1.964V7.07a.785.785 0 10-1.571 0v1.965a.394.394 0 01-.393.393H1.964a.394.394 0 01-.393-.393V2.75c0-.216.177-.393.393-.393h1.964a.785.785 0 100-1.571H1.964z"
+        fill="#C3C2D4"
+      />
+    </svg>
+  );
+}
+
+const MemoExternalLinkSVG = React.memo(ExternalLinkSVG);
 
 // export const referralCodeAtom = atomWithStorage('referral-code5', '');
 
@@ -169,17 +183,13 @@ function App() {
             <Warning
               body={
                 <div>
-                  Buffer{' '}
-                  <a href="https://app.buffer.finance/" target="_blank">
-                    <span className="underline underline-offset-2">V2.6 </span>
+                  <a
+                    className="flex items-baseline"
+                    href="https://mirror.xyz/0xc730FbdFEb3e9dF76008A19962963cA4A2bd8de2/ao87r3b-1Apd_3SAknXX-rHlhspngxCvscaX5vk4JCI"
+                  >
+                    Stake your LP over 90 days to 💥Nitro Boost Your BLP APR by
+                    Up to 1.4x.  Learn More&nbsp; <MemoExternalLinkSVG />
                   </a>
-                  is live on Mainnet.{' '}
-                  {/* <a href="https://app.buffer.finance/" target="_blank">
-                    <span className="underline underline-offset-2">
-                      Learn More
-                    </span>{' '}
-                    <ShareIcon className=" scale-[0.65] w-fit inline" />
-                  </a> */}
                 </div>
               }
               closeWarning={() => {}}
@@ -197,3 +207,5 @@ function App() {
 }
 
 export default App;
+
+// import * as React from 'react';
