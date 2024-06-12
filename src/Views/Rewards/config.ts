e32cef51033f84eeba1a9530fe450c9da2f6f0b8
gakshita <curie@buffer.finance>
2024-06-12 11:16:22 +0530

    Fix: Season start date changed

--- src/Views/Rewards/config.ts
+++ src/Views/Rewards/config.ts
@@ -1,4 +1,4 @@
-export const startWeekId = 2839;
+export const startWeekId = 2840;
 export const rebatesAddress = '';
 export const competitionRewardAddress =
   '0x9e53fF83b10B7531f0EeD7010b1bF847DaB88267';
