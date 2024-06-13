d7c3e29d7c8b426f4786fe072d269caa50493cf3
maxwell-1999 <maxwell@buffer.finance>
2024-06-13 16:16:04 +0530

    WIP

--- src/Views/LpRewards/Components/BoostYield/Transactions/helpers.ts
+++ src/Views/LpRewards/Components/BoostYield/Transactions/helpers.ts
@@ -2,5 +2,5 @@ export function convertToNumberOfMonthsAndDays(seconds: number) {
   // conver the given seconds in to number of months and days
   const months = Math.floor(seconds / 2629746);
   const days = Math.floor((seconds % 2629746) / 86400);
-  return { months: 0, days: months * 30 + days };
+  return { months: 0, days: seconds / (24 * 60 * 60) };
 }
