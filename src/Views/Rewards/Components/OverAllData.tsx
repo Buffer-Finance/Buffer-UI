689675f1e940d95ec720a5d67b68e2ab724b5aeb
maxwell-1999 <maxwell@buffer.finance>
2024-06-20 14:03:25 +0530

    Leaderboard rewards changes done

--- src/Views/Rewards/Components/OverAllData.tsx
+++ src/Views/Rewards/Components/OverAllData.tsx
@@ -1,4 +1,4 @@
-import { divide } from '@Utils/NumString/stringArithmatics';
+import { divide, toFixed } from '@Utils/NumString/stringArithmatics';
 import { TableAligner } from '@Views/V2-Leaderboard/Components/TableAligner';
 import { getTimestampFromWeekId } from '@Views/V2-Leaderboard/Leagues/WinnersByPnl/getWeekId';
 import { Skeleton } from '@mui/material';
@@ -81,7 +81,7 @@ export const OverAllData: React.FC<{
               ) : (
                 <span>
                   <span className="text-[#FFFFFF]">
-                    {divide(data?.volume ?? '0', 6)}
+                    {toFixed(divide(data?.volume ?? '0', 6), 2)}
                   </span>
                   <span className="text-[#7F87A7]"> USDC</span>
                 </span>
