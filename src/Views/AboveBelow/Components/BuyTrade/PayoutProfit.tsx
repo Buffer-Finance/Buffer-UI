a487905928b29b7b1b69ff3f1eb6a5c95cff9593
maxwell-1999 <maxwell@buffer.finance>
2024-06-25 21:06:34 +0530

    Payout recalculated with multiplier.

--- src/Views/AboveBelow/Components/BuyTrade/PayoutProfit.tsx
+++ src/Views/AboveBelow/Components/BuyTrade/PayoutProfit.tsx
@@ -1,5 +1,6 @@
 import { toFixed } from '@Utils/NumString';
 import {
+  add,
   divide,
   gt,
   multiply,
@@ -32,6 +33,7 @@ export const PayoutProfit = ({}: {}) => {
       divide(subtract(contracts, paidAmount), paidAmount) as string,
       2
     );
+    const multiplier = toFixed(add(divide(totalPayout, '100'), '1'), 1);
     return (
       <div className="flex-sbw text-f14 my-3 mb-4 sm:my-[0px] ">
         <div className="text-f12 b1200:text-f14 items-start flex-start flex-col b1200: flex wrap b1200:items-center text-2 b1200:flex-row">
@@ -42,7 +44,7 @@ export const PayoutProfit = ({}: {}) => {
               data={contracts}
               unit={tradeToken}
             />
-            {toFixed(totalPayout, 0) + '%'}
+            {multiplier + 'x'}
           </RowGap>
         </div>
         <div className="text-f12 b1200:text-f14 items-end flex-col flex-start wrap flex text-2  gap-y-1 b1200:items-center b1200:flex-row">
