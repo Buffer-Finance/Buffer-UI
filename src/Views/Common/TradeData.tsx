c0c2d391cd5b499a25c6947f53cf4a059450030a
maxwell-1999 <maxwell@buffer.finance>
2024-07-11 15:06:59 +0530

    AB Support

--- src/Views/Common/TradeData.tsx
+++ src/Views/Common/TradeData.tsx
@@ -7,7 +7,14 @@ const TradeData: React.FC<{ trade: TradeType }> = ({ trade }) => {
       onClick={(e) => {
         e.stopPropagation();
         window.open(
-          `https://api-v2.6.buffer.finance/trades/${trade.router}/${trade.queue_id}/?environment=42161`,
+          `https://api-v2.6.buffer.finance/trades/${trade.router}/${
+            trade.queue_id
+          }/?environment=42161&product_id=${
+            trade.router.toLowerCase() ==
+            '0x94582981c3be6092b912265C2d2cE172e7f9c3B1'.toLowerCase()
+              ? 'xyz'
+              : 'abc'
+          }`,
           '_blank'
         );
       }}
