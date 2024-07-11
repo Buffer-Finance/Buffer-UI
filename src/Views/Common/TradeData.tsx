51f1def65eded611928bd3bf4d699382c317e1f3
maxwell-1999 <maxwell@buffer.finance>
2024-07-11 15:03:44 +0530

    Added Trade explorer button

--- src/Views/Common/TradeData.tsx
+++ src/Views/Common/TradeData.tsx
@@ -4,12 +4,13 @@ import { TradeType } from '@Views/ABTradePage/type';
 const TradeData: React.FC<{ trade: TradeType }> = ({ trade }) => {
   return (
     <button
-      onClick={() =>
+      onClick={(e) => {
+        e.stopPropagation();
         window.open(
           `https://api-v2.6.buffer.finance/trades/${trade.router}/${trade.queue_id}/?environment=42161`,
           '_blank'
-        )
-      }
+        );
+      }}
     >
       <InfoIcon tooltip="Get the full trade information." sm />
     </button>
