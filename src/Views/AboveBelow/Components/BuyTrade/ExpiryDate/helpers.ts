532d4a3320cc637801177d7bbc7f695d43e677ab
maxwell-1999 <maxwell@buffer.finance>
2024-07-26 12:25:11 +0530

    Discard expiry less than 12 hrs.

--- src/Views/AboveBelow/Components/BuyTrade/ExpiryDate/helpers.ts
+++ src/Views/AboveBelow/Components/BuyTrade/ExpiryDate/helpers.ts
@@ -34,16 +34,14 @@ export function getTimestamps(date = Date.now()) {
   }
 
   //add next day if the start of day is less than 36 hours away
-  if (
-    // startOfDayTimestamp - currentTimestamp > 43200000 &&
-    startOfDayTimestamp - currentTimestamp <
-    129600000
-  ) {
+  if (startOfDayTimestamp - currentTimestamp < 129600000) {
     const nextDay = new Date(date);
     nextDay.setUTCDate(nextDay.getUTCDate() + 1);
     nextDay.setUTCHours(8, 0, 0, 0);
     const nextDayTimestamp = nextDay.getTime();
-    timestamps.push(nextDayTimestamp);
+    if (nextDayTimestamp - currentTimestamp > 43200000) {
+      timestamps.push(nextDayTimestamp);
+    }
   }
 
   //add 2nd day
